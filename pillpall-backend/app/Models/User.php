8141db1a35e71d005e6291f689219c6f19ccbb12
RashaM <rashamassoud1998@gmail.com>
2023-04-15 23:42:51 +0300

    Imported the models for the relation between User and Result

--- pillpall-backend/app/Models/User.php
+++ pillpall-backend/app/Models/User.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
+use App\Models\Result;
+
 class User extends Authenticatable implements JWTSubject{
     use HasFactory, Notifiable;
 
