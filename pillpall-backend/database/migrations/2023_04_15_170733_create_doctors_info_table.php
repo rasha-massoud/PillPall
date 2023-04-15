ccc4dc702eb7dae9d2af05ffd13a6731ca8fc76b
RashaM <rashamassoud1998@gmail.com>
2023-04-15 20:17:32 +0300

    Created doctors_info migration table

---
+++ pillpall-backend/database/migrations/2023_04_15_170733_create_doctors_info_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+
+    public function up(): void{
+        Schema::create('doctors_info', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('image');
+            $table->integer('phone_number');
+            $table->date('dob');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('working_hours');
+            $table->string('major');
+            $table->string('certificates');
+            $table->string('expertise');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void{
+        Schema::dropIfExists('doctors_info');
+    }
+};
