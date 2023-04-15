d97e7482feb83333f44fb799605906a2970354c6
RashaM <rashamassoud1998@gmail.com>
2023-04-15 20:03:40 +0300

    Cleaned the patients_info table

--- pillpall-backend/database/migrations/2023_04_15_162848_create_patients_info_table.php
+++ pillpall-backend/database/migrations/2023_04_15_162848_create_patients_info_table.php
@@ -4,10 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
+return new class extends Migration{
+
+    public function up(): void{
         Schema::create('patients_info', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
@@ -39,8 +38,7 @@ return new class extends Migration
         });
     }
 
-    public function down(): void
-    {
+    public function down(): void{
         Schema::dropIfExists('patients_info');
     }
 };
