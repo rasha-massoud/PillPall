522f5167a993fba5aed82fa2d732d278f44a138a
RashaM <rashamassoud1998@gmail.com>
2023-04-16 01:23:31 +0300

    Added the routes for the AuthController functions

--- pillpall-backend/routes/api.php
+++ pillpall-backend/routes/api.php
@@ -3,17 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(["prefix" => "v0.0.0"], function(){
+    Route::post('/register',[AuthController:: class, "register"]);
+    Route::post('/login',[AuthController:: class, "login"]);
+    Route::post('/logout',[AuthController:: class, "logout"]);
+    Route::post('/refresh',[AuthController:: class, "refresh"]);
+    
+    Route::group(["middleware" => ["auth:api"]], function(){
+             
+    });
 });
