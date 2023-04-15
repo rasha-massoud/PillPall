392d510e23b0fe8dd3c3cf30902c5dbff42782a3
RashaM <rashamassoud1998@gmail.com>
2023-04-15 20:45:07 +0300

    Created results migration table

---
+++ pillpall-backend/database/migrations/2023_04_15_174034_create_results_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+
+    public function up(): void{
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->date('testing_date');
+            $table->string('file_name');
+            $table->string('description');
+            $table->string('file_path');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void{
+        Schema::dropIfExists('results');
+    }
+};
