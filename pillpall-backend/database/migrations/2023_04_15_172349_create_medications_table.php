5acec4354c20297e79a6f20b19c3d88ef2ae962c
RashaM <rashamassoud1998@gmail.com>
2023-04-15 20:37:36 +0300

    Created medications migration table

---
+++ pillpall-backend/database/migrations/2023_04_15_172349_create_medications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+
+    public function up(): void{
+        Schema::create('medications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->integer('dose_quantity');
+            $table->integer('price_per_month');
+            $table->string('instructions');
+            $table->string('days');
+            $table->time('timing');
+            $table->tinyInteger('first_of_each_month');
+            $table->tinyInteger('on_demand');
+            $table->string('image');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void{
+        Schema::dropIfExists('medications');
+    }
+};
