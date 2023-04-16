609d54294c3d546abe129a7bde5574e6a107f2a1
RashaM <rashamassoud1998@gmail.com>
2023-04-16 13:58:44 +0300

    Modified the create_report function in the PatientController

--- pillpall-backend/app/Http/Controllers/PatientController.php
+++ pillpall-backend/app/Http/Controllers/PatientController.php
@@ -3,43 +3,63 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use Exception;
 
 use App\Models\User;
+use App\Models\PatientsInfo;
 
 class PatientController extends Controller{
     
     public function create_report(Request $request){
-        $user= new User();
+        // if (!$request->user()) {
+        //     return response()->json(['error' => 'Unauthorized'], 401);
+        // }
 
-        $user->user_id = $request->user_id;
-        $user->image = $request->image;
-        $user->phone_number = $request->phone_number;
-        $user->dob = $request->dob;
-        $user->address = $request->address;
-        $user->gender = $request->gender;
-        $user->blood_type = $request->blood_type;
-        $user->height = $request->height;
-        $user->weight = $request->weight;
-        $user->emergency_name = $request->emergency_name;
-        $user->emergency_number = $request->emergency_number;
-        $user->emergency_email = $request->emergency_email;
-        $user->emergency_contact_relation = $request->emergency_contact_relation;
-        $user->body_temperature = $request->body_temperature;
-        $user->pulse_rate = $request->pulse_rate;
-        $user->respiration_rate = $request->respiration_rate;
-        $user->systolic_blood_pressure = $request->systolic_blood_pressure;
-        $user->chronic_conditions = $request->chronic_conditions;
-        $user->past_surgeries = $request->past_surgeries;
-        $user->family_medical_history = $request->family_medical_history;
-        $user->allergies = $request->allergies;
-        $user->life_style_habits = $request->life_style_habits;
-        $user->medications = $request->medications;
+        try {
+            $patient= new PatientsInfo();
 
-        $user->save();
+            $patient->user_id = Auth::id();
+            $patient->phone_number = $request->phone_number;
+            $patient->dob = $request->dob;
+            $patient->address = $request->address;
+            $patient->gender = $request->gender;
+            $patient->blood_type = $request->blood_type;
+            $patient->height = $request->height;
+            $patient->weight = $request->weight;
+            $patient->emergency_name = $request->emergency_name;
+            $patient->emergency_number = $request->emergency_number;
+            $patient->emergency_email = $request->emergency_email;
+            $patient->emergency_contact_relation = $request->emergency_contact_relation;
+            $patient->body_temperature = $request->body_temperature;
+            $patient->pulse_rate = $request->pulse_rate;
+            $patient->respiration_rate = $request->respiration_rate;
+            $patient->systolic_blood_pressure = $request->systolic_blood_pressure;
+            $patient->chronic_conditions = $request->chronic_conditions;
+            $patient->past_surgeries = $request->past_surgeries;
+            $patient->family_medical_history = $request->family_medical_history;
+            $patient->allergies = $request->allergies;
+            $patient->life_style_habits = $request->life_style_habits;
+            $patient->medications = $request->medications;
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Pateint Report created successfully'
-        ]);
+            if ($request->hasFile('image')) {
+                $imagePath = $request->file('image')->store('images');
+                $patient->image = $imagePath;
+            }
+
+            $patient->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Pateint Report created successfully'
+            ]);
+
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while creating the patient report.' . $e->getMessage()
+            ]);
+        }
     }
 }
--- pillpall-backend/routes/api.php
+++ pillpall-backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PatientController;
 
 Route::group(["prefix" => "v0.0.0"], function(){
     Route::post('/register',[AuthController:: class, "register"]);
@@ -12,6 +13,7 @@ Route::group(["prefix" => "v0.0.0"], function(){
     Route::post('/refresh',[AuthController:: class, "refresh"]);
     
     Route::group(["middleware" => ["auth:api"]], function(){
-             
+        Route::post('/create_report',[PatientController:: class, "create_report"]);
+
     });
 });
