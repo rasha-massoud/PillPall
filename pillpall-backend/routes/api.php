a30c6274e26c7a85aa852445208084a90277ab26
RashaM <rashamassoud1998@gmail.com>
2023-04-16 16:26:39 +0300

    Added the route to the add_medicine API

--- pillpall-backend/routes/api.php
+++ pillpall-backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\MedicalController;
 
 Route::group(["prefix" => "v0.0.0"], function(){
     Route::post('/register',[AuthController:: class, "register"]);
@@ -14,6 +15,8 @@ Route::group(["prefix" => "v0.0.0"], function(){
     
     Route::group(["middleware" => ["auth:api"]], function(){
         Route::post('/create_update_report',[PatientController:: class, "create_update_report"]);
+
+        Route::post('/add_medicine',[MedicalController:: class, "add_medicine"]);
 
     });
 });
