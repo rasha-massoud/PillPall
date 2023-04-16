802ace1a4a90b17c6df1023e755351b031a61958
RashaM <rashamassoud1998@gmail.com>
2023-04-17 02:18:22 +0300

    Modified the nearby pharmacies API

--- pillpall-backend/app/Http/Controllers/PharmacyController.php
+++ pillpall-backend/app/Http/Controllers/PharmacyController.php
@@ -9,23 +9,32 @@ use GuzzleHttp\Client;
 class PharmacyController extends Controller{
 
     public function get_nearby_pharmacies($lat, $lng) {
-        $client = new Client();
-        $response = $client->get("https://nominatim.openstreetmap.org/search.php?q=pharmacy&format=json&lat={$lat}&lon={$lng}&radius=5000");
-        $data = json_decode($response->getBody());
-    
-        $pharmacies = array();
-        $count = 0;
-        foreach ($data as $location) {
-            if ($location->class == 'amenity' && $location->type == 'pharmacy' && $count < 6) {
-                $pharmacies[] = array(
-                    'name' => $location->display_name,
-                    'latitude' => $location->lat,
-                    'longitude' => $location->lon
-                );
-                $count++;
+
+        try{
+            $client = new Client();
+            $response = $client->get("https://nominatim.openstreetmap.org/search.php?q=pharmacy&format=json&lat={$lat}&lon={$lng}&radius=5000");
+            $data = json_decode($response->getBody());
+        
+            $pharmacies = array();
+            $count = 0;
+            foreach ($data as $location) {
+                if ($location->class == 'amenity' && $location->type == 'pharmacy' && $count < 6) {
+                    $pharmacies[] = array(
+                        'name' => $location->display_name,
+                        'latitude' => $location->lat,
+                        'longitude' => $location->lon
+                    );
+                    $count++;
+                }
             }
+        
+            return response()->json($pharmacies);
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while getting the nearby pharmacies.' 
+            ]);
         }
-    
-        return response()->json($pharmacies);
+        
     }
 }
