b9ea02462d2abe3e34669d581537057be676be14
RashaM <rashamassoud1998@gmail.com>
2023-04-16 16:55:35 +0300

    Editted the route to the add_edit_medicine API

--- pillpall-backend/routes/api.php
+++ pillpall-backend/routes/api.php
@@ -16,7 +16,7 @@ Route::group(["prefix" => "v0.0.0"], function(){
     Route::group(["middleware" => ["auth:api"]], function(){
         Route::post('/create_update_report',[PatientController:: class, "create_update_report"]);
 
-        Route::post('/add_medicine',[MedicalController:: class, "add_medicine"]);
+        Route::post('/add_edit_medicine',[MedicalController:: class, "add_edit_medicine"]);
         Route::post('/delete_medicine',[MedicalController:: class, "delete_medicine"]);
 
     });
