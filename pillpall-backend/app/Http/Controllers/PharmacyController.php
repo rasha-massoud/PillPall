0ba57d5215ffb2d0184e77d19c4db4eab23d8ea9
RashaM <rashamassoud1998@gmail.com>
2023-04-17 02:16:26 +0300

    Added the get_nearby_pharmacies API

--- pillpall-backend/app/Http/Controllers/PharmacyController.php
+++ pillpall-backend/app/Http/Controllers/PharmacyController.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PharmacyController extends Controller
-{
-    //
+use GuzzleHttp\Client;
+
+class PharmacyController extends Controller{
+
+    public function get_nearby_pharmacies($lat, $lng) {
+        $client = new Client();
+        $response = $client->get("https://nominatim.openstreetmap.org/search.php?q=pharmacy&format=json&lat={$lat}&lon={$lng}&radius=5000");
+        $data = json_decode($response->getBody());
+    
+        $pharmacies = array();
+        $count = 0;
+        foreach ($data as $location) {
+            if ($location->class == 'amenity' && $location->type == 'pharmacy' && $count < 6) {
+                $pharmacies[] = array(
+                    'name' => $location->display_name,
+                    'latitude' => $location->lat,
+                    'longitude' => $location->lon
+                );
+                $count++;
+            }
+        }
+    
+        return response()->json($pharmacies);
+    }
 }
