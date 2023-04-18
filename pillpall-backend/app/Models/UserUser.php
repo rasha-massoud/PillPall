4f46bd58039ccc34e605cdb7ba8dab8e6487ea17
RashaM <rashamassoud1998@gmail.com>
2023-04-18 21:47:30 +0300

    Created the relation between patints and doctors

--- pillpall-backend/app/Models/UserUser.php
+++ pillpall-backend/app/Models/UserUser.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class UserUser extends Model
-{
-    use HasFactory;
+class UserUser extends Model{
+
+    protected $table = 'user_user';
+    
+    protected $fillable = [
+        'patient_id',
+        'doctor_id',
+    ];
+
+    public function patient(){
+        return $this->belongsTo(User::class, 'patient_id');
+    }
+
+    public function doctor(){
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
 }
