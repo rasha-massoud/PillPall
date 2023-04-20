9e1dc6b8f3c7e7e0268d0dc043be6c1262dcba65
RashaM <rashamassoud1998@gmail.com>
2023-04-20 13:37:48 +0300

    Updated the chatbot API, to respond to medical questions

--- pillpall-backend/app/Http/Controllers/ChatbotController.php
+++ pillpall-backend/app/Http/Controllers/ChatbotController.php
@@ -17,20 +17,33 @@ class ChatbotController extends Controller{
     
             if ($question_type == 'question'){
                 $question= $request->prompt;
-                $prompt= "Act as a doctor, '{$question}'. If '{$question}' is not a medical question, reply only with 'Not a medical question'";
+                $prompt = $question;
 
                 $response = Http::withHeaders([
                     'Content-Type' => 'application/json',
                     'Authorization' => 'Bearer ' . env('CHATGPT_API_KEY'),
                 ])->post('https://api.openai.com/v1/completions', [
                     'prompt' => $prompt,
-                    'max_tokens' => 127,
-                    'temperature' => 0.8,
+                    'max_tokens' => 100,
+                    'temperature' => 0.7,
                     'model' => 'davinci',
                 ]);
 
-                $jsonResponse = json_encode($response->json());
+                $answer = $response->json()['choices'][0]['text'];
 
+                if (preg_match('/(medicine|treatment|disease|symptom|diagnosis|doctor|prescription|pharmacy|hospital|patient)/i', $answer)) {
+                    return response()->json([
+                        'prompt' => $prompt,
+                        'status' => 'success',
+                        'message' => 'Answer retrieved successfully.',
+                        'answer' => $answer
+                    ]);
+                } else {
+                    return response()->json([
+                        'status' => 'error',
+                        'message' => 'Not a medical question'
+                    ]);
+                }
         
             } else if ($question_type == 'replacement'){
     
@@ -42,12 +55,6 @@ class ChatbotController extends Controller{
     
             }
     
-            return response()->json([
-                'prompt' => $prompt,
-                'status' => 'success',
-                'message' => 'Answer retrieved successfully.',
-                'answer' => $response->json()
-            ]);
         } catch (Exception $e){
             return response()->json([
                 'status' => 'error',
