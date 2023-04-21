e5741b9c9b2f245032980b389a556af1b3f53b98
RashaM <rashamassoud1998@gmail.com>
2023-04-21 13:07:10 +0300

    Added middleware to AdminController constructor to restrict access to admin users only.

--- pillpall-backend/app/Http/Controllers/AdminController.php
+++ pillpall-backend/app/Http/Controllers/AdminController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
-{
-    //
+class AdminController extends Controller{
+    
+    public function __construct(){
+
+        $this->middleware('auth');
+        $this->middleware(function ($request, $next) {
+            if (Auth::user()->role !== 'admin') {
+                return response()->json(['error' => 'Unauthorized'], 401);
+            }
+            return $next($request);
+        });
+    }
+    
+    public function get_all_users(){
+
+
+    }
 }
