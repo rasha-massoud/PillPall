251293a0ac7e3c614ff2467ff3d68d4c2cbb80da
RashaM <rashamassoud1998@gmail.com>
2023-04-21 14:38:03 +0300

    Added the relationships in the ApprovedDoctor Model

--- pillpall-backend/app/Models/ApprovedDoctor.php
+++ pillpall-backend/app/Models/ApprovedDoctor.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ApprovedDoctor extends Model
-{
+class ApprovedDoctor extends Model{
     use HasFactory;
+
+    public function doctor(){
+        return $this->belongsTo(User::class, 'doctor_id');
+    }
+    
+    public function admin(){
+        return $this->belongsTo(User::class, 'admin_id');
+    }
 }
