50a094e3fc101195e00005ae80756ac123a5eb32
RashaM <rashamassoud1998@gmail.com>
2023-04-22 02:13:27 +0300

    Created the change_password API

--- pillpall-backend/app/Http/Controllers/PasswordController.php
+++ pillpall-backend/app/Http/Controllers/PasswordController.php
@@ -3,8 +3,44 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class PasswordController extends Controller
-{
-    //
+use Exception;
+
+use App\Models\User;
+
+class PasswordController extends Controller{
+    
+    public function change_password(){
+
+        try{
+
+            $request->validate([
+                'password' => 'required|string|min:8|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/',
+                'confirm_password' => 'required|string|same:password',
+            ]);
+        
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Validation Error',
+                ]);
+            }
+        
+            $user = Auth::user();
+            $user->password = Hash::make($request->password);
+            $user->save();
+        
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Password changed successfully.',
+            ]);
+
+        } catch (Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while changing the password.' 
+            ]);
+        }
+    }
 }
