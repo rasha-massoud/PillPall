cb155e045faf8e3342839f54a338c2e9e40781d7
RashaM <rashamassoud1998@gmail.com>
2023-04-22 01:51:13 +0300

    Added the route to the get_patients API

--- pillpall-backend/routes/api.php
+++ pillpall-backend/routes/api.php
@@ -47,6 +47,7 @@ Route::group(["prefix" => "v0.0.0"], function(){
         Route::post('/approve',[AdminController:: class, "approve"]);
         Route::get('/get_all_users',[AdminController:: class, "get_all_users"]);
         Route::get('/get_report/{user_id}',[AdminController:: class, "get_report"]);
+        Route::get('/get_patients',[AdminController:: class, "get_patients"]);
 
     });
 });
