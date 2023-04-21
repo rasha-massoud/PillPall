96a3fb9e58fb53567776455b2a77ce79191c1e5b
RashaM <rashamassoud1998@gmail.com>
2023-04-21 14:20:50 +0300

    Created the approved_doctors migration table

---
+++ pillpall-backend/database/migrations/2023_04_21_111659_create_approved_doctors_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration{
+    
+    public function up(): void{
+        Schema::create('approved_doctors', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('doctor_id');
+            $table->unsignedBigInteger('admin_id');
+            $table->string('status')->default('unapproved');
+            $table->timestamps();
+
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('admin_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unique(['doctor_id', 'admin_id']);
+            
+        });
+    }
+
+    public function down(): void{
+        Schema::dropIfExists('approved_doctors');
+    }
+};
