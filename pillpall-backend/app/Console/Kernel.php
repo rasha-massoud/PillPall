af8300b4f31df07cb4b39d3e58bc6325179a0430
RashaM <rashamassoud1998@gmail.com>
2023-04-24 00:57:20 +0300

    Registered and Scheduled the command

--- pillpall-backend/app/Console/Kernel.php
+++ pillpall-backend/app/Console/Kernel.php
@@ -5,21 +5,17 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
-    /**
-     * Define the application's command schedule.
-     */
-    protected function schedule(Schedule $schedule): void
-    {
-        // $schedule->command('inspire')->hourly();
+class Kernel extends ConsoleKernel{
+    
+    protected function schedule(Schedule $schedule): void{
+        $schedule->command('medication:remind')->everyMinute();
     }
 
-    /**
-     * Register the commands for the application.
-     */
-    protected function commands(): void
-    {
+    protected $commands = [
+        \App\Console\Commands\SendMedicationReminders::class,
+    ];
+    
+    protected function commands(): void{
         $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
