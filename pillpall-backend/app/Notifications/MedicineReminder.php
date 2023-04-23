14181248033129ea41e4532c34e69bed61ecc191
RashaM <rashamassoud1998@gmail.com>
2023-04-24 00:43:10 +0300

    Definined the toFcm methoud

--- pillpall-backend/app/Notifications/MedicineReminder.php
+++ pillpall-backend/app/Notifications/MedicineReminder.php
@@ -7,6 +7,9 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
+use NotificationChannels\Fcm\FcmMessage;
+use NotificationChannels\Fcm\FcmNotification;
+
 class MedicineReminder extends Notification{
 
     use Queueable;
@@ -28,5 +31,19 @@ class MedicineReminder extends Notification{
         return [
             
         ];
+    }
+
+    public function toFcm($notifiable){
+        return FcmMessage::create()
+            ->setData([
+                'title' => 'Medicine Reminder',
+                'body' => $this->message,
+                'click_action' => config('app.url'),
+            ])
+            ->setNotification(FcmNotification::create()
+                ->setTitle('Medicine Reminder')
+                ->setBody($this->message))
+            ->setPriority('high')
+            ->setTimeToLive(0);
     }
 }
