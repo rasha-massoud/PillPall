9088926f7ecdd6087bf5410019a4819e31873dd2
RashaM <rashamassoud1998@gmail.com>
2023-04-24 00:45:12 +0300

    Cleaned the SendMedicationReminders Command

--- pillpall-backend/app/Console/Commands/SendMedicationReminders.php
+++ pillpall-backend/app/Console/Commands/SendMedicationReminders.php
@@ -4,27 +4,13 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class SendMedicationReminders extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+class SendMedicationReminders extends Command{
+
     protected $signature = 'app:send-medication-reminders';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Command description';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        //
+    public function handle(){
+        
     }
 }
