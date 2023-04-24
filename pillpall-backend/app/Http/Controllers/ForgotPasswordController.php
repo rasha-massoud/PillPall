ec022a002d4a0decf03eeab2729e579c78c2eac1
RashaM <rashamassoud1998@gmail.com>
2023-04-24 14:24:45 +0300

    Created the reset_password API

--- pillpall-backend/app/Http/Controllers/ForgotPasswordController.php
+++ pillpall-backend/app/Http/Controllers/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
 
 class ForgotPasswordController extends Controller{
     
@@ -20,6 +21,31 @@ class ForgotPasswordController extends Controller{
    
     }
 
+    public function reset_password(Request $request){
+        
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string|min:8|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$/',
+            'confirm_password' => 'required|string|same:password',
+            'token' => 'required|string',
+        ]);
 
+        $response = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $response == Password::PASSWORD_RESET
+                    ? response()->json(['message' => 'Password reset successfully.'])
+                    : response()->json(['message' => 'Unable to reset password.'], 500);
+    }
 
 }
