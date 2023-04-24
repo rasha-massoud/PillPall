c0a0dc88020f12c0bc990463bf08e93716323396
RashaM <rashamassoud1998@gmail.com>
2023-04-24 14:17:03 +0300

    Created the forget_password API

--- pillpall-backend/app/Http/Controllers/ForgotPasswordController.php
+++ pillpall-backend/app/Http/Controllers/ForgotPasswordController.php
@@ -3,9 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller{
     
+    public function forgotPassword(Request $request){
+        $request->validate(['email' => 'required|email']);
+ 
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? response()->json(['message' => 'Reset password link sent on your email id.'])
+                    : response()->json(['message' => 'Unable to send reset link.'], 500);
+   
+    }
 
 
 
