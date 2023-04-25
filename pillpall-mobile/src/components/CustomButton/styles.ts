8c55b20d7f5d212cbdf46a8662a9e7def004e634
RashaM <rashamassoud1998@gmail.com>
2023-04-26 01:02:12 +0300

    Frontend: Updated the CustomButton stylesheet

---
+++ pillpall-mobile/src/components/CustomButton/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#0084CA',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 30,
+    width: '32%',
+    height: '6%',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'medium',
+    fontSize: 14,
+    textTransform: 'uppercase',
+  },
+});
+
+export default styles;
