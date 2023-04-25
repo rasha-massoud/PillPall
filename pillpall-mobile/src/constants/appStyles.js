f7b0ce0bdba9ffb1b09ba4fc95732e9b78939186
RashaM <rashamassoud1998@gmail.com>
2023-04-26 02:17:22 +0300

    Frontend: Created the appStyles.js file that contains the default font styles of the whole app

---
+++ pillpall-mobile/src/constants/appStyles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+
+const appStyles = StyleSheet.create({
+  mainTitle: {
+    fontWeight: 'medium',
+    fontSize: 34,
+    letterSpacing: '0.73%', 
+  },
+  pageTitle: {
+    fontWeight: 'medium',
+    fontSize: 24,
+    letterSpacing: '0%', 
+  },
+  subTitle: {
+    fontWeight: 'medium',
+    fontSize: 20,
+    letterSpacing: '0.75%', 
+  },
+  subTitle2: {
+    fontWeight: 'regular',
+    fontSize: 16,
+    letterSpacing: '0.93%', 
+  },  
+  body1: {
+    fontWeight: 'regular',
+    fontSize: 16,
+    letterSpacing: '3.12%', 
+  },
+  body2: {
+    fontWeight: 'regular',
+    fontSize: 14,
+    letterSpacing: '1.78%', 
+  },
+  button: {
+    fontWeight: 'medium',
+    fontSize: 14,
+    letterSpacing: '8.9%', 
+  },
+
+});
+
+export default appStyles;
