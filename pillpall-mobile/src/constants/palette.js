10f83f63bd555a6361e4ec5f54384d7c5bfe2ddd
RashaM <rashamassoud1998@gmail.com>
2023-04-26 01:11:47 +0300

    Frontend: Created the palette.js file

---
+++ pillpall-mobile/src/constants/palette.js
@@ -0,0 +1,30 @@
+export const colors = {
+    blue: '#0084CA',
+    green: '#01FF6D',
+    black: '#000000',
+    white: '#FFFFFF',
+    light_gray: '#F0F0F0',
+    dark_gray: '#C9C9C9',
+}
+
+export const shadows = {
+    sm: {
+        color: '#606470',
+        opacity: 0.15,
+        x: 0,
+        y: 3,
+        blur: 4
+    },
+    md: {
+        color: '#606470',
+        opacity: 0.15,
+        x: 0,
+        y: 5,
+        blur: 20
+    }
+}
+
+export default {
+    colors,
+    shadows
+}
