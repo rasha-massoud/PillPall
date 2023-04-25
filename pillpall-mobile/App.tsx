99d3d56c77abf50ad673df2d0e1eaedf78f3b5f1
RashaM <rashamassoud1998@gmail.com>
2023-04-26 00:54:26 +0300

    Frontend: Updated the App.tsx file for testing purposes

--- pillpall-mobile/App.tsx
+++ pillpall-mobile/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomButton from './src/components/CustomButton';
+import React from 'react';
 
 export default function App() {
+  const handlePress = () => {
+    console.log('Button pressed');
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CustomButton buttonprops={{ title: 'Press me', onPress: handlePress }} />
     </View>
   );
 }
