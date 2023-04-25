396a8c60d92331366c6c325e5006248bfb9cd150
RashaM <rashamassoud1998@gmail.com>
2023-04-26 01:15:39 +0300

    Frontend: Created the font.js file

---
+++ pillpall-mobile/src/constants/font.js
@@ -0,0 +1,14 @@
+import { useFonts, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+
+export const fonts = {
+    regular: 'Roboto_400Regular',
+    medium: 'Roboto_500Medium',
+    bold: {
+      fontFamily: 'Roboto_500Medium',
+      fontWeight: 'bold',
+    },
+};
+
+export default {
+    fonts
+}
