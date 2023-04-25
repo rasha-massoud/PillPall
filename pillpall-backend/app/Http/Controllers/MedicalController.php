facf9f1324589c5c24e198fb34c4ca3e36ed722f
RashaM <rashamassoud1998@gmail.com>
2023-04-25 12:05:58 +0300

    Updated the add_file_number API accordingly

--- pillpall-backend/app/Http/Controllers/MedicalController.php
+++ pillpall-backend/app/Http/Controllers/MedicalController.php
@@ -137,11 +137,12 @@ class MedicalController extends Controller{
             $file->doctor_name = $request->doctor_name;
             $file->address = $request->address;
             $file->file_number = $request->file_number;
-            
-            if ($request->hasFile('image')) {
-                $imagePath = $request->file('image')->store('images');
-                $file->image = $imagePath;
-            }
+            $file->image = null; 
+
+            // if ($request->hasFile('image')) {
+            //     $imagePath = $request->file('image')->store('images');
+            //     $file->image = $imagePath;
+            // }
     
             $file->save();
     
