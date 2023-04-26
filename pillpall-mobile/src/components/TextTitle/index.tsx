90da42ff668b480485a4a9ac52b6d733e4f0b4af
RashaM <rashamassoud1998@gmail.com>
2023-04-27 01:55:43 +0300

    Frontend: Updated the TextTitle Component

--- pillpall-mobile/src/components/TextTitle/index.tsx
+++ pillpall-mobile/src/components/TextTitle/index.tsx
@@ -3,11 +3,12 @@ import React, { FC } from 'react';
 import appStyles from '../../constants/appStyles';
 
 interface TextTitleProps {
-    title: string;
+  title: string;
+  color?: string;
 }
 
 const TextTitle: FC<TextTitleProps> = (props) => {
-  return <Text style={appStyles.mainTitle}>{props.title}</Text>
+  return <Text style={[appStyles.mainTitle, {color: props.color|| 'black'}]}>{props.title}</Text>
 };
 
 export default TextTitle;
