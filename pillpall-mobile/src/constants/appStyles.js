609eaed6d1fbce34791ecc51834fd7014ed0dfea
RashaM <rashamassoud1998@gmail.com>
2023-04-26 04:20:22 +0300

    Frontend: Modified the letterSpacing in the appStyles.js

--- pillpall-mobile/src/constants/appStyles.js
+++ pillpall-mobile/src/constants/appStyles.js
@@ -5,37 +5,37 @@ const appStyles = StyleSheet.create({
   mainTitle: {
     fontFamily: fonts.medium,
     fontSize: 34,
-    letterSpacing: '0.73%', 
+    letterSpacing: 0.73, 
   },
   pageTitle: {
     fontFamily: fonts.medium,
     fontSize: 24,
-    letterSpacing: '0%', 
+    letterSpacing: 0, 
   },
   subTitle: {
     fontFamily: fonts.medium,
     fontSize: 20,
-    letterSpacing: '0.75%', 
+    letterSpacing: 0.75, 
   },
   subTitle2: {
     fontFamily: fonts.regular,
     fontSize: 16,
-    letterSpacing: '0.93%', 
+    letterSpacing: 0.93, 
   },  
   body1: {
     fontFamily: fonts.regular,
     fontSize: 16,
-    letterSpacing: '3.12%', 
+    letterSpacing: 3.12, 
   },
   body2: {
     fontFamily: fonts.regular,
     fontSize: 14,
-    letterSpacing: '1.78%', 
+    letterSpacing: 1.78%, 
   },
   button: {
     fontFamily: fonts.medium,
     fontSize: 14,
-    letterSpacing: '8.9%', 
+    letterSpacing: 8.9, 
   },
 
 });
