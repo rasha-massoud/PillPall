6a6375402568b6319f8e76d8e05c58ddf8da95aa
RashaM <rashamassoud1998@gmail.com>
2023-04-27 02:00:40 +0300

    Frontend: Created the Welcome Screen

--- pillpall-mobile/src/screens/welcome/index.tsx
+++ pillpall-mobile/src/screens/welcome/index.tsx
@@ -4,7 +4,7 @@ import CustomButton from '../../components/CustomButton';
 import TextTitle from '../../components/TextTitle';
 import SubTitleText from '../../components/SubTitleText';
 import Body1Text from '../../components/Body1Text';
-
+import { colors } from '../../constants/palette'
 import styles from './styles'
 
 const Welcome: FC = () => {
@@ -16,7 +16,7 @@ const Welcome: FC = () => {
     return (
     
     <SafeAreaView style={styles.container}>
-        <TextTitle title='Hi PillPaller' style={styles.text}></TextTitle>
+        <TextTitle title='Hi PillPaller!' color= {colors.blue} />
         <SubTitleText title='Welcome to PillPall.'></SubTitleText>
 
         <Image
@@ -25,7 +25,7 @@ const Welcome: FC = () => {
         />
 
         <Body1Text context="As a patient, your health information is critical to help us understand your medical needs and provide the best care possible. To ensure we have a comprehensive understanding of your health status, we have a six-step process that will help us gather all the necessary information. Each step is designed to collect different aspects of your health information.
-            By completing each step, we can provide you with personalized and effective care. Let's walk through each step together."></Body1Text>
+By completing each step, we can provide you with personalized and effective care. Let's walk through each step together."></Body1Text>
 
         <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
 
