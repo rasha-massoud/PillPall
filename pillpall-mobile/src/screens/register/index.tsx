c5403004b0f267a019c75ccec06d19a60d7e6df5
RashaM <rashamassoud1998@gmail.com>
2023-04-27 01:16:26 +0300

    Frontend: Updated the Register Screen

--- pillpall-mobile/src/screens/register/index.tsx
+++ pillpall-mobile/src/screens/register/index.tsx
@@ -27,7 +27,7 @@ const Register: FC = () => {
         <TextInputwithLabel label="Password" textinputprops={{ secureTextEntry: true }} />
         <TextInputwithLabel label="ConfirmPassword" textinputprops={{ secureTextEntry: true }} />
 
-        <TwoCustomButton containerStyle={{ alignSelf: 'center', marginBottom: '10%' }} buttonprops2={{ title: "Cancel", onPress: () => console.log('Cancel') }} buttonprops1={{ title: "Signup", onPress: () => console.log('Signup') }}></TwoCustomButton>
+        <TwoCustomButton containerStyle={{ alignSelf: 'center'}} buttonprops2={{ title: "Cancel", onPress: () => console.log('Cancel') }} buttonprops1={{ title: "Signup", onPress: () => console.log('Signup') }}></TwoCustomButton>
 
         <LoginSignupSwitch style={{ marginTop: '10%' }}textTitle="Already have an account?" action="Login" onPress={handleLoginPress}></LoginSignupSwitch>
     </SafeAreaView>
