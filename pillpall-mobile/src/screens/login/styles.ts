9c697b9657c3817dbe866b8194b901087594b921
RashaM <rashamassoud1998@gmail.com>
2023-04-26 16:40:24 +0300

    Frontend: Removed the unneccessary styles from the styles.ts

--- pillpall-mobile/src/screens/login/styles.ts
+++ pillpall-mobile/src/screens/login/styles.ts
@@ -6,8 +6,6 @@ import { Dimensions } from 'react-native';
 
 interface Styles {
     container: ViewStyle;
-    inputContainer: ViewStyle;
-    input: TextStyle;
     buttonContainer: ViewStyle;
     forgotPassword: TextStyle;
     forgotPasswordText: TextStyle;
@@ -25,22 +23,6 @@ const styles = StyleSheet.create<Styles>({
         marginTop: '45%',
         width: LOGIN_PAGE_WIDTH,
         marginHorizontal: width * 0.05, 
-    },
-    inputContainer: {
-        height: 35,
-        marginVertical: 12,
-        width: '100%',
-        marginBottom: 35,
-    },
-    input: {
-        borderWidth: 1,
-        borderColor: 'lightgrey',
-        padding: 10,
-        borderRadius: 5,
-        fontFamily: appStyles.body2.fontFamily,
-        fontSize: appStyles.body2.fontSize,
-        color: colors.darker_gray,
-        height: 40,
     },
     buttonContainer: {
         justifyContent: 'center',
