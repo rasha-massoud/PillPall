251e7a86ef115716235f1d1de50c5b6dca6175df
RashaM <rashamassoud1998@gmail.com>
2023-04-26 11:47:42 +0300

    Frontend: Updated the CustomButton to accept style

--- pillpall-mobile/src/components/CustomButton/index.tsx
+++ pillpall-mobile/src/components/CustomButton/index.tsx
@@ -1,22 +1,24 @@
-import { Button, ButtonProps, Text, TouchableOpacity, View } from 'react-native'
-import React, { FC } from 'react'
-import styles from './styles'
+import { Button, ButtonProps, Text, TouchableOpacity, View, StyleProp, ViewStyle, TextStyle } from 'react-native';
+import React, { FC } from 'react';
+import styles from './styles';
 
-interface CustomButtonProps  {
-  buttonprops: ButtonProps
+interface CustomButtonProps {
+  buttonprops: ButtonProps;
+  containerStyle?: StyleProp<ViewStyle>;
+  textStyle?: StyleProp<TextStyle>;
 }
 
 const CustomButton: FC<CustomButtonProps> = (props) => {
   return (
     <>
-        <TouchableOpacity  
-            onPress={props.buttonprops.onPress} 
-            style={styles.buttonContainer}>
-
-            <Text style={styles.buttonText}>{props.buttonprops.title}</Text>
-        </TouchableOpacity>
+      <TouchableOpacity
+        onPress={props.buttonprops.onPress}
+        style={[styles.buttonContainer, props.containerStyle]}
+      >
+        <Text style={[styles.buttonText, props.textStyle]}>{props.buttonprops.title}</Text>
+      </TouchableOpacity>
     </>
-  )
-}
+  );
+};
 
-export default CustomButton
+export default CustomButton;
