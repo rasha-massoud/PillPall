4f6ae38f1a09555b44948d49aede5a1cc672318c
RashaM <rashamassoud1998@gmail.com>
2023-04-28 00:49:57 +0300

    Frontend: Updated the Register screen

--- pillpall-mobile/src/screens/register/index.tsx
+++ pillpall-mobile/src/screens/register/index.tsx
@@ -1,15 +1,21 @@
-import React, { FC } from 'react'
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
-import CustomButton from '../../components/CustomButton';
+import React, { FC, useState} from 'react'
+import { SafeAreaView } from 'react-native';
 import TextInputwithLabel from '../../components/TextInputwithLabel';
 import TextTitle from '../../components/TextTitle';
 import SubTitleText from '../../components/SubTitleText';
 import LoginSignupSwitch from '../../components/LoginSignupSwitch';
 import TwoCustomButton from '../../components/TwoCustomButton';
+import RoleCheckBox from '../../components/RoleCheckBox';
 
 import styles from './styles'
 
 const Register: FC = () => {
+
+    const [selectedRole, setSelectedRole] = useState<string>();
+
+    const handleRoleSelect = (role: string) => {
+        setSelectedRole(role);
+    };
 
     const handleLoginPress = () => {
         // Navigate to Login in screen
@@ -26,6 +32,8 @@ const Register: FC = () => {
 
         <TextInputwithLabel label="Password" placeholder='Enter your Password' textinputprops={{ secureTextEntry: true }} />
         <TextInputwithLabel label="ConfirmPassword" placeholder='Re-enter your Password' textinputprops={{ secureTextEntry: true }} />
+        
+        <RoleCheckBox selectedRole={selectedRole} onRoleSelect={handleRoleSelect} />
 
         <TwoCustomButton containerStyle={{ alignSelf: 'center'}} buttonprops2={{ title: "Cancel", onPress: () => console.log('Cancel') }} buttonprops1={{ title: "Signup", onPress: () => console.log('Signup') }}></TwoCustomButton>
 
