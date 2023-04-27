db01e6c6e73099e02cbb5076070ed707f9ff646b
RashaM <rashamassoud1998@gmail.com>
2023-04-28 00:10:33 +0300

    Frontend: Updated the SecureTextEntry in the ContactInfo screen

--- pillpall-mobile/src/screens/contactinfo/index.tsx
+++ pillpall-mobile/src/screens/contactinfo/index.tsx
@@ -29,7 +29,7 @@ const ContactInfo: FC = () => {
 
     <TextInputwithLabel label="Phone Number" keyboardType="numeric" placeholder='Enter your Phone Number' textinputprops={{ secureTextEntry: false }}/>
 
-    <TextInputwithLabel label="Date of Birth" placeholder='Enter your Day of Birth' textinputprops={{ secureTextEntry: true }} />
+    <TextInputwithLabel label="Date of Birth" placeholder='Enter your Day of Birth' textinputprops={{ secureTextEntry: false }} />
 
     <TextInputwithLabel label="Address" keyboardType="numeric" placeholder='Enter your Address' textinputprops={{ secureTextEntry: false }}/>
 
