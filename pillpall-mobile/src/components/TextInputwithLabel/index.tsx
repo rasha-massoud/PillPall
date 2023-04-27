1c65b45b0c3c3659b3d716d2b9fb5eadacd6f010
RashaM <rashamassoud1998@gmail.com>
2023-04-28 00:06:15 +0300

    Frontend: Wrapped the TextInputwithLabel Component with TouchableWithoutFeedback

--- pillpall-mobile/src/components/TextInputwithLabel/index.tsx
+++ pillpall-mobile/src/components/TextInputwithLabel/index.tsx
@@ -1,8 +1,9 @@
-import { Text, View, KeyboardTypeOptions, TextStyle, TextInput, TextInputProps } from 'react-native';
-import React, { FC } from 'react';
+import { Text, View, KeyboardTypeOptions, TouchableWithoutFeedback, TextInput, TextInputProps } from 'react-native';
+import React, { FC, useRef } from 'react';
 import styles from './styles';
 import appStyles from '../../constants/appStyles';
 import { colors } from '../../constants/palette';
+import { Keyboard } from 'react-native';
 
 interface TextInputwithLabelProps {
   textinputprops: TextInputProps;
@@ -14,20 +15,31 @@ interface TextInputwithLabelProps {
 type EmailKeyboardType = 'email-address';
 
 const TextInputwithLabel: FC<TextInputwithLabelProps> = (props) => {
+  const inputRef = useRef<TextInput>(null);
+
+  const dismissKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
     <>
-        <View style={styles.inputContainer}>
+        <TouchableWithoutFeedback onPress={dismissKeyboard}>
+
+          <View style={styles.inputContainer}>
             <Text style={appStyles.body1}>{props.label}</Text>
-            <TextInput 
-                style={styles.input}
-                secureTextEntry
-                autoCapitalize="none"
-                keyboardType={props.keyboardType}
-                {...props.textinputprops}
-                placeholderTextColor={colors.dark_gray}
-                placeholder={props.placeholder}
-            />
-        </View>
+              <TextInput 
+                  ref={inputRef}
+                  style={styles.input}
+                  secureTextEntry
+                  autoCapitalize="none"
+                  keyboardType={props.keyboardType}
+                  {...props.textinputprops}
+                  placeholderTextColor={colors.dark_gray}
+                  placeholder={props.placeholder}
+              />
+          </View>
+        </TouchableWithoutFeedback>
+
     </>
   );
 };
