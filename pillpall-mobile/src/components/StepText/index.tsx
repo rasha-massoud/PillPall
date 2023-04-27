03458c825d5555cd66f842e06ccdc4ba11342aa3
RashaM <rashamassoud1998@gmail.com>
2023-04-28 00:29:37 +0300

    Frontend: Created the StepText Component

---
+++ pillpall-mobile/src/components/StepText/index.tsx
@@ -0,0 +1,15 @@
+import { Text } from 'react-native';
+import React, { FC } from 'react';
+import appStyles from '../../constants/appStyles';
+import { colors } from '../../constants/palette';
+
+interface StepTextProps {
+    title: string;
+    color?: string;
+}
+
+const StepText: FC<StepTextProps> = (props) => {
+    return <Text style={[appStyles.subTitle2, {color: colors.blue, marginBottom: 12}]}>{props.title}</Text>
+};
+
+export default StepText;
