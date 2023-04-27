b18eee0bf4ee5eff2a98e656d3036f94f97c0b57
RashaM <rashamassoud1998@gmail.com>
2023-04-28 02:50:02 +0300

    Frontend: Created the Reset Password Screen

---
+++ pillpall-mobile/src/screens/resetpassword/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react'
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import CustomButton from '../../components/CustomButton';
+import TextInputwithLabel from '../../components/TextInputwithLabel';
+import TextTitle from '../../components/TextTitle';
+
+import styles from './styles'
+
+const ResetPassword: FC = () => {
+
+    return (
+    
+    <SafeAreaView style={styles.container}>
+        <Image
+            source={require('../../../assets/resetpass.png')}
+            style={styles.image}
+        />
+
+        <TextTitle title='Please, enter a new password below.'></TextTitle>
+
+        <TextInputwithLabel label="Password" placeholder='Enter your Password' textinputprops={{ secureTextEntry: true }} />
+        <TextInputwithLabel label="ConfirmPassword" placeholder='Re-enter your Password' textinputprops={{ secureTextEntry: true }} />
+        
+        <CustomButton containerStyle={{ alignSelf: 'center' }} buttonprops={{ title: "Login", onPress: () => console.log('Login') }}  />
+    
+    </SafeAreaView>
+  );
+};
+
+export default ResetPassword;
