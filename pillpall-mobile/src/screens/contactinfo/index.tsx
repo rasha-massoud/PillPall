f1a99a0941d1d7afb930f9593150fa12cf999f7c
RashaM <rashamassoud1998@gmail.com>
2023-04-27 15:46:31 +0300

    Frontend: Updated the AddImage Component

---
+++ pillpall-mobile/src/screens/contactinfo/index.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react'
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import CustomButton from '../../components/CustomButton';
+import PageTitle from '../../components/PageTitle';
+import SubTitleText from '../../components/SubTitleText';
+import Body1Text from '../../components/Body1Text';
+import { colors } from '../../constants/palette';
+import TextInputwithLabel from '../../components/TextInputwithLabel';
+import AddImage from '../../components/AddImage';
+
+import styles from './styles';
+
+const Welcome: FC = () => {
+
+    return (
+    
+    <SafeAreaView style={styles.container}>
+        <PageTitle title='Hi PillPaller!' color= {colors.blue} />
+        <SubTitleText title='Step 1'></SubTitleText>
+
+        <Body1Text context="To provide you with the best care possible, we need your contact information, such as your phone number, date of birth, address, and gender. This information helps us keep in touch with you and keep your medical records up-to-date."></Body1Text>
+
+        <AddImage></AddImage>
+
+        <TextInputwithLabel label="Phone Number" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
+
+        <TextInputwithLabel label="Date of Birth" placeholder='Enter your Password' textinputprops={{ secureTextEntry: true }} />
+
+        <TextInputwithLabel label="Address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
+
+        <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
+
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
