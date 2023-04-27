3636925b661e6c31278edba55ea9536c3d7e1dca
RashaM <rashamassoud1998@gmail.com>
2023-04-27 14:31:55 +0300

    Frontend: Updated the folders for the AddImage Component

---
+++ pillpall-mobile/src/components/PageTitle/index.tsx
@@ -0,0 +1,14 @@
+import { Text } from 'react-native';
+import React, { FC } from 'react';
+import appStyles from '../../constants/appStyles';
+
+interface TextTitleProps {
+  title: string;
+  color?: string;
+}
+
+const TextTitle: FC<TextTitleProps> = (props) => {
+  return <Text style={[appStyles.pageTitle, {color: props.color|| 'black'}]}>{props.title}</Text>
+};
+
+export default TextTitle;
