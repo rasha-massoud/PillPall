2773f151878590ffc6c9aedd2e811ceebfbafcfa
RashaM <rashamassoud1998@gmail.com>
2023-04-27 22:07:46 +0300

    Frontend: Updated the ContactInfo screen to have the gender checkbox

--- pillpall-mobile/src/screens/contactinfo/index.tsx
+++ pillpall-mobile/src/screens/contactinfo/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import CustomButton from '../../components/CustomButton';
 import PageTitle from '../../components/PageTitle';
@@ -7,30 +7,36 @@ import Body1Text from '../../components/Body1Text';
 import { colors } from '../../constants/palette';
 import TextInputwithLabel from '../../components/TextInputwithLabel';
 import AddImage from '../../components/AddImage';
+import GenderCheckBox from '../../components/GenderCheckBox';
 
 import styles from './styles';
 
 const ContactInfo: FC = () => {
+  const [gender, setGender] = useState<string | undefined>();
 
-    return (
-    
-    <SafeAreaView style={styles.container}>
-        <PageTitle title='Contact Information' />
-        <SubTitleText title='Step 1' color={colors.blue}></SubTitleText>
+  const handleGenderSelect = (selectedGender: string) => {
+    setGender(selectedGender);
+  };
+  return (
+  
+  <SafeAreaView style={styles.container}>
+    <PageTitle title='Contact Information' />
+    <SubTitleText title='Step 1' color={colors.blue}></SubTitleText>
 
-        <Body1Text context="To provide you with the best care possible, we need your contact information, such as your phone number, date of birth, address, and gender. This information helps us keep in touch with you and keep your medical records up-to-date."></Body1Text>
+    <Body1Text context="To provide you with the best care possible, we need your contact information, such as your phone number, date of birth, address, and gender. This information helps us keep in touch with you and keep your medical records up-to-date."></Body1Text>
 
-        <AddImage></AddImage>
+    <AddImage></AddImage>
 
-        <TextInputwithLabel label="Phone Number" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Phone Number" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
 
-        <TextInputwithLabel label="Date of Birth" placeholder='Enter your Password' textinputprops={{ secureTextEntry: true }} />
+    <TextInputwithLabel label="Date of Birth" placeholder='Enter your Password' textinputprops={{ secureTextEntry: true }} />
 
-        <TextInputwithLabel label="Address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
 
-        <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
+    <GenderCheckBox selectedGender={gender} onGenderSelect={handleGenderSelect} />
+    <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
 
-    </SafeAreaView>
+  </SafeAreaView>
   );
 };
 
