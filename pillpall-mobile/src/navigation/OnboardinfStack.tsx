488b5016d4d76d4aa9a74e710f0b58fbb34abbe2
RashaM <rashamassoud1998@gmail.com>
2023-04-29 02:33:02 +0300

    Frontend: Modified the OnboardingStack

--- pillpall-mobile/src/navigation/OnboardinfStack.tsx
+++ pillpall-mobile/src/navigation/OnboardinfStack.tsx
@@ -5,7 +5,11 @@ import Register from "../screens/register";
 const OnboardingStack = () => {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen name="LoginScreen" component={Login} />
       <Stack.Screen name="SignupScreen" component={Register} />
     </Stack.Navigator>
