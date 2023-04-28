a080e877455a8e172ac0639aafb1f26c61c57816
RashaM <rashamassoud1998@gmail.com>
2023-04-29 02:15:14 +0300

    Frontend: Created the OnboardingStack

---
+++ pillpall-mobile/src/navigation/OnboardinfStack.tsx
@@ -0,0 +1,15 @@
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "../screens/login";
+import Register from "../screens/register";
+
+const OnboardingStack = () => {
+  const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="LoginScreen" component={Login} />
+      <Stack.Screen name="SignupScreen" component={Register} />
+    </Stack.Navigator>
+  );
+};
+
+export default OnboardingStack;
