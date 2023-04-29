dce7b2e862adac222975a7b131e3c06dee715821
RashaM <rashamassoud1998@gmail.com>
2023-04-29 14:58:53 +0300

    Frontend: Updated the App.tsx for testing purposes

--- pillpall-mobile/App.tsx
+++ pillpall-mobile/App.tsx
@@ -26,9 +26,9 @@ export default function App() {
   }, []);
   
   return (
-    <NavigationContainer>
+    <View style={styles.container}>
       <MedicationSchedule/>
-    </NavigationContainer>
+    </View>
   );
 }
 
