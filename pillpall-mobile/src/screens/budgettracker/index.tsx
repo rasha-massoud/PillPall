6149d66d170441e00b046b2c436fda45e66ed9eb
RashaM <rashamassoud1998@gmail.com>
2023-04-30 02:45:42 +0300

    Frontend: Updated the Budget Tracker Screen

--- pillpall-mobile/src/screens/budgettracker/index.tsx
+++ pillpall-mobile/src/screens/budgettracker/index.tsx
@@ -1,12 +1,28 @@
-import React, { FC, useState } from 'react'
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { FC, useState, useEffect } from 'react'
+import { SafeAreaView, Image } from 'react-native';
 import NavBar from '../../components/NavBar';
-import styles from './styles';
-import { HERE_API_KEY } from '../../../config';
+import SubTitleText from '../../components/SubTitleText';
+import PieChartComponent from '../../components/BudgetPieChart';
+import axios from 'axios';
 
+import styles from './styles';
 
 const BudgetTracker: FC = () => {
-    
+
+    const [budgetData, setBudgetData] = useState<{ [month: string]: number }>({});
+
+    useEffect(() => {
+      const fetchBudgetData = async () => {
+        try {
+          const response = await axios.get('http://192.168.0.103:8000/api/v0.0.0/budget_tracker');
+          setBudgetData(response.data.prices);
+        } catch (error) {
+          console.log('An Error occured');
+        }
+      };
+      fetchBudgetData();
+    }, []);
+
     return (
     
         <SafeAreaView style={styles.container}>
@@ -17,6 +33,9 @@ const BudgetTracker: FC = () => {
                 source={require('../../../assets/budgettrackerscreen.png')}
                 style={styles.image}
             />
+            <SubTitleText title='Last Three Months' />
+
+            <PieChartComponent data={budgetData} />
 
 
         </SafeAreaView>
