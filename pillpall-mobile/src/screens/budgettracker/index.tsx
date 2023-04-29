dccbc469eb937398f24efe112f9606bf8976643c
RashaM <rashamassoud1998@gmail.com>
2023-04-30 02:54:26 +0300

    Frontend: Updated the Budget Tracker Screen

--- pillpall-mobile/src/screens/budgettracker/index.tsx
+++ pillpall-mobile/src/screens/budgettracker/index.tsx
@@ -2,7 +2,7 @@ import React, { FC, useState, useEffect } from 'react'
 import { SafeAreaView, Image } from 'react-native';
 import NavBar from '../../components/NavBar';
 import SubTitleText from '../../components/SubTitleText';
-import PieChartComponent from '../../components/BudgetPieChart';
+import BudgetPieChart from '../../components/BudgetPieChart';
 import axios from 'axios';
 
 import styles from './styles';
@@ -35,7 +35,7 @@ const BudgetTracker: FC = () => {
             />
             <SubTitleText title='Last Three Months' />
 
-            <PieChartComponent data={budgetData} />
+            <BudgetPieChart data={budgetData} />
 
 
         </SafeAreaView>
