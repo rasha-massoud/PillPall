68c4bffe38d4b47a453d227dcd03b720cd9af6e9
RashaM <rashamassoud1998@gmail.com>
2023-04-29 19:51:24 +0300

    Frontend: Updated the TimingCheckList Component

--- pillpall-mobile/src/components/TimingCheckList/index.tsx
+++ pillpall-mobile/src/components/TimingCheckList/index.tsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Body1Text from '../Body1Text';
 
 import styles from './styles';
+import appStyles from '../../constants/appStyles';
 
 type Timing = '6:00' | '8:00' | '10:00' | '12:00' | '14:00' | '16:00' | '18:00' | '20:00' | '22:00';
 
@@ -12,24 +14,25 @@ type ChecklistProps = {
 };
 
 const Checklist: React.FC<ChecklistProps> = ({ timings, selectedTiming, onSelectTiming }) => {
-  const handleTimingPress = (timing: Timing) => {
-    if (timing !== selectedTiming) {
-      onSelectTiming(timing);
-    }
-  };
+    const handleTimingPress = (timing: Timing) => {
+        if (timing !== selectedTiming) {
+        onSelectTiming(timing);
+        }
+    };
 
-  return (
-    <View>
-      {timings.map((timing) => (
-        <TouchableOpacity key={timing} onPress={() => handleTimingPress(timing)}>
-          <View style={[styles.timingItem, timing === selectedTiming && styles.selectedTimingItem]}>
-            <Text style={[styles.timingText, timing === selectedTiming && styles.selectedTimingText]}>
-              {timing}
-            </Text>
-          </View>
-        </TouchableOpacity>
-      ))}
-    </View>
+    return (
+        <View>
+            <Body1Text context='Timing' />
+            {timings.map((timing) => (
+                <TouchableOpacity key={timing} onPress={() => handleTimingPress(timing)}>
+                <View style={[styles.timingItem, timing === selectedTiming && styles.selectedTimingItem]}>
+                    <Text style={[styles.timingText, appStyles.body2,  timing === selectedTiming && styles.selectedTimingText]}>
+                    {timing}
+                    </Text>
+                </View>
+                </TouchableOpacity>
+            ))}
+        </View>
   );
 };
 
