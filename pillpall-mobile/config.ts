f9fad899fe8203fa4af5e367bd7572a2414870cd
RashaM <rashamassoud1998@gmail.com>
2023-05-01 23:56:40 +0300

    Frontend: Deleted the past saved global URL

--- pillpall-mobile/config.ts
+++ pillpall-mobile/config.ts
@@ -1,6 +1,5 @@
 import Config from 'react-native-config';
 
 const HERE_API_KEY = Config.HERE_API_KEY;
-const API_URL = 'http://192.168.0.107:8000/api/v0.0.0';
 
-export default { HERE_API_KEY, API_URL };
+export default HERE_API_KEY;
