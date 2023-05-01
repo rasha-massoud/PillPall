8de0a23ff50008f74e09d66406a527735dff2724
RashaM <rashamassoud1998@gmail.com>
2023-05-01 22:25:54 +0300

    FrontEnd: Defined the backend APIs URL as a global constant

--- pillpall-mobile/config.ts
+++ pillpall-mobile/config.ts
@@ -1,5 +1,6 @@
 import Config from 'react-native-config';
 
 const HERE_API_KEY = Config.HERE_API_KEY;
+const API_URL = 'http://192.168.0.107:8000/api/v0.0.0';
 
-export { HERE_API_KEY };
+export default { HERE_API_KEY, API_URL };
