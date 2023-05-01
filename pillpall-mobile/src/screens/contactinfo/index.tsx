1c1ac230775761180714082984222d5a12940aa1
RashaM <rashamassoud1998@gmail.com>
2023-05-02 01:56:53 +0300

    Frontend: Updated the Contact Info Screen

--- pillpall-mobile/src/screens/contactinfo/index.tsx
+++ pillpall-mobile/src/screens/contactinfo/index.tsx
@@ -8,15 +8,45 @@ import TextInputwithLabel from '../../components/TextInputwithLabel';
 import AddImage from '../../components/AddImage';
 import GenderCheckBox from '../../components/GenderCheckBox';
 import StepText from '../../components/StepText';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import API_URL from '../../constants/url';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import styles from './styles';
 
 const ContactInfo: FC = () => {
+
+  const [address, setAddress] = useState<string>('');
+  const [dob, setDob] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+
   const [gender, setGender] = useState<string | undefined>();
 
-  const handleGenderSelect = (selectedGender: string) => {
+  const handlePhoneNumberChange = async (value: string) => {
+    setPhoneNumber(value);
+    await AsyncStorage.setItem('phone_number', value);
+  }
+
+  const handleAddressChange = async (value: string) => {
+    setAddress(value);
+    await AsyncStorage.setItem('address', value);
+  }
+
+  const handleDoBChange = async (value: string) => {
+    setDob(value);
+    await AsyncStorage.setItem('dob', dob);
+  }
+  
+  const handleGenderSelect = async (selectedGender: string) => {
     setGender(selectedGender);
+    await AsyncStorage.setItem('gender', selectedGender);
   };
+
+  const handleContinuePress = () => {
+    //Navigate to Step 2
+  }
+
   return (
   
   <SafeAreaView style={styles.container}>
@@ -27,14 +57,15 @@ const ContactInfo: FC = () => {
 
     <AddImage></AddImage>
 
-    <TextInputwithLabel label="Phone Number" keyboardType="numeric" placeholder='Enter your Phone Number' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Phone Number" keyboardType="numeric" placeholder='Enter your Phone Number' textinputprops={{ secureTextEntry: false }} onChangeText= {handlePhoneNumberChange} />
 
-    <TextInputwithLabel label="Date of Birth" placeholder='YYYY-MM-DD' textinputprops={{ secureTextEntry: false }} />
+    <TextInputwithLabel label="Date of Birth" placeholder='YYYY-MM-DD' textinputprops={{ secureTextEntry: false }} onChangeText= {handleDoBChange} />
 
-    <TextInputwithLabel label="Address" placeholder='Enter your Address' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Address" placeholder='Enter your Address' textinputprops={{ secureTextEntry: false }} onChangeText= {handleAddressChange} />
 
     <GenderCheckBox selectedGender={gender} onGenderSelect={handleGenderSelect} />
-    <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 20 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
+
+    <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 20 }} buttonprops={{ title: "Continue", onPress: handleContinuePress }} />
 
   </SafeAreaView>
   );
