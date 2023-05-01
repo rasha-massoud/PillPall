363d19b6a00e6188fc6f9bcbea713f0e7a32da31
RashaM <rashamassoud1998@gmail.com>
2023-05-02 02:19:16 +0300

    Frontend: Updated the Emergency Contact Info Screen

--- pillpall-mobile/src/screens/emergencycontact/index.tsx
+++ pillpall-mobile/src/screens/emergencycontact/index.tsx
@@ -6,10 +6,43 @@ import Body1Text from '../../components/Body1Text';
 import { colors } from '../../constants/palette';
 import TextInputwithLabel from '../../components/TextInputwithLabel';
 import StepText from '../../components/StepText';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import API_URL from '../../constants/url';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import styles from './styles';
 
 const EmergencyContact: FC = () => {
+
+  const [name, setName] = useState<string>('');
+  const [phone_number, setPhoneNumber] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [relation, setRelation] = useState<string>('');
+
+  const handleNameChange = async (value: string) => {
+    setName(value);
+    await AsyncStorage.setItem('name', value);
+  }
+
+  const handlePhoneNumberChange = async (value: string) => {
+    setPhoneNumber(value);
+    await AsyncStorage.setItem('phone_number', value);
+  }
+
+  const handleEmailChange = async(value: string) => {
+    setEmail(value);
+    await AsyncStorage.setItem('email', value);
+  }
+
+  const handleRelationChange = async (value: string) => {
+    setRelation(value);
+    await AsyncStorage.setItem('relation', value);
+  }
+
+  const handleContinuePress = () => {
+    //Navigate to Step 4
+  }
 
   return (
   
@@ -19,15 +52,15 @@ const EmergencyContact: FC = () => {
 
     <Body1Text context="In case of an emergency, we need the name, phone number, email address, and relation of a trusted friend or family member who we can contact. Providing this information helps us ensure your safety and well-being."></Body1Text>
 
-    <TextInputwithLabel label="Name" placeholder='Enter your Name' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Name" placeholder='Enter your Name' textinputprops={{ secureTextEntry: false }} onChangeText= {handleNameChange} />
 
-    <TextInputwithLabel label="Phone Number" keyboardType="numeric" placeholder='Enter your Phone Number' textinputprops={{ secureTextEntry: false }} />
+    <TextInputwithLabel label="Phone Number" keyboardType="numeric" placeholder='Enter your Phone Number' textinputprops={{ secureTextEntry: false }} onChangeText= {handlePhoneNumberChange} />
 
-    <TextInputwithLabel label="Email" keyboardType="email-address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Email" keyboardType="email-address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }} onChangeText= {handleEmailChange} />
     
-    <TextInputwithLabel label="Relation" placeholder='Enter your Relation with the Contact' textinputprops={{ secureTextEntry: false }}/>
+    <TextInputwithLabel label="Relation" placeholder='Enter your Relation with the Contact' textinputprops={{ secureTextEntry: false }} onChangeText= {handleRelationChange} />
 
-    <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
+    <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: handleContinuePress }}  />
 
   </SafeAreaView>
   );
