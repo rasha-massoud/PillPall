8b20f6e37c324e68872d9d765c0915a8ee35e080
RashaM <rashamassoud1998@gmail.com>
2023-05-02 00:40:52 +0300

    Frontend: Linked the Forgot Password Screen to the Backened

--- pillpall-mobile/src/screens/forgotpassword/index.tsx
+++ pillpall-mobile/src/screens/forgotpassword/index.tsx
@@ -1,30 +1,76 @@
-import React, { FC } from 'react'
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { FC, useState} from 'react'
+import { SafeAreaView, Alert , Image } from 'react-native';
 import TwoCustomButton from '../../components/TwoCustomButton';
 import TextInputwithLabel from '../../components/TextInputwithLabel';
 import TextTitle from '../../components/TextTitle';
 import SubTitleText from '../../components/SubTitleText';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+import API_URL from '../../constants/url';
 
 import styles from './styles'
 
 const ForgotPassword: FC = () => {
 
+    const [email, setEmail] = useState<string>('');
+
+    const validateEmail = (email: string): boolean => {
+        const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        return regex.test(email.trim());
+    }
+
+    const handleEmailChange = (value: string) => {
+        setEmail(value);
+    };
+
+    const handleResetPress = async () => {
+        if (!validateEmail(email)) {
+            Alert.alert('Invalid email', 'Please enter a valid email address.');
+            return;
+        }
+
+        const data = new FormData();
+        data.append('email', email);
+
+
+        const endpoint = 'password/forgot';
+        await axios.post(`${API_URL}${endpoint}`, data, {
+            headers: {
+                'Content-Type': "application/x-www-form-urlencoded",
+                'Accept': 'application/json',
+            },
+        })
+        .then((response) => {
+            Alert.alert(
+                'Success',
+                'An email has been sent to your email address to reset your password.',
+                [
+                  { text: 'OK' }
+                ],
+                { cancelable: false }
+              );
+        })
+        .catch((error) => {
+            console.error('An error occurred or invalid email');
+        });
+    }
+
     return (
     
-    <SafeAreaView style={styles.container}>
-        <Image
-            source={require('../../../assets/email.png')}
-            style={styles.image}
-        />
+        <SafeAreaView style={styles.container}>
+            <Image
+                source={require('../../../assets/email.png')}
+                style={styles.image}
+            />
 
-        <TextTitle title='Forgot Password?'></TextTitle>
-        <SubTitleText title='Please enter your email address below to reset your email password'></SubTitleText>
+            <TextTitle title='Forgot Password?'></TextTitle>
+            <SubTitleText title='Please fill your email address below to reset your email password'></SubTitleText>
 
-        <TextInputwithLabel label="Email" keyboardType="email-address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false }}/>
+            <TextInputwithLabel label='Email' keyboardType="email-address" placeholder='Enter your Email' textinputprops={{ secureTextEntry: false}} onChangeText= {handleEmailChange} />
 
-        <TwoCustomButton containerStyle={{ alignSelf: 'center'}} buttonprops2={{ title: "Cancel", onPress: () => console.log('Cancel') }} buttonprops1={{ title: "Signup", onPress: () => console.log('Signup') }}></TwoCustomButton>
-        
-    </SafeAreaView>
+            <TwoCustomButton containerStyle={{ alignSelf: 'center'}} buttonprops2={{ title: "Cancel", onPress: () => console.log('Cancel') }} buttonprops1={{ title: "Reset", onPress: handleResetPress }} />
+            
+        </SafeAreaView>
   );
 };
 
