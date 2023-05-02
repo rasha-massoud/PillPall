9a44b59cc83f25775354efc078aa6fa2aa009ee1
RashaM <rashamassoud1998@gmail.com>
2023-05-02 18:12:02 +0300

    Frontend: Updated the reportSlice

--- pillpall-mobile/src/store/slices/reportSlice.tsx
+++ pillpall-mobile/src/store/slices/reportSlice.tsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     name: "",
     email: "",
-    image: "",
+    image: null,
     phone_number: "",
     dob: "",
     address: "",
