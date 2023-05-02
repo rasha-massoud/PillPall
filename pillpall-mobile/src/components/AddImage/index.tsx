a6ddf35fc4abe308a90cbc8b28bdcffc086d9e60
RashaM <rashamassoud1998@gmail.com>
2023-05-02 11:50:47 +0300

    Frontend: Updated the AddImage Component

--- pillpall-mobile/src/components/AddImage/index.tsx
+++ pillpall-mobile/src/components/AddImage/index.tsx
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import React, { FC, useState } from 'react'
+import { View, TouchableOpacity, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { ImagePickerResult } from 'expo-image-picker/build/ImagePicker.types';
 import { Ionicons } from '@expo/vector-icons';
 
 import styles from './styles';
 
-const AddImage = () => {
-    const [image, setImage] = useState<string | null>(null);
+type AddImageProps = {
+    setImage: (image: string | null) => void;
+};
+
+const AddImage: FC<AddImageProps> = (props) => {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
     const pickImage = async () => {
         let result: ImagePickerResult = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ImagePicker.MediaTypeOptions.All,
-        allowsEditing: true,
-        aspect: [4, 3],
-        quality: 1,
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
         });
-
+    
         console.log(result);
-
+    
         if (!result.canceled && result.assets && result.assets.length > 0) {
-        setImage(result.assets[0].uri);
+            setSelectedImage(result.assets[0].uri);
+            props.setImage(result.assets[0].uri);
         }
     };
 
     return (
         <View style={styles.container}>
-        {image ? (
-            <Image source={{ uri: image }} style={styles.image} />
-        ) : (
             <TouchableOpacity onPress={pickImage}>
-            <View style={styles.editIconContainer}>
+            {selectedImage ? (
+                <Image source={{ uri: selectedImage }} style={styles.image} />
+            ) : (
+                <View style={styles.editIconContainer}>
                 <Ionicons name="pencil" size={24} color="#fff" />
-            </View>
+                </View>
+            )}
             </TouchableOpacity>
-        )}
-        </View>
+      </View>
     );
 };
 
