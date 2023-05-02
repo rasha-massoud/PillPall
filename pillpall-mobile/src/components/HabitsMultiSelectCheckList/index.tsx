edbc4a845aad57c46a971b5c560e766551d0840a
RashaM <rashamassoud1998@gmail.com>
2023-05-02 18:39:56 +0300

    Frontend: Updated the HabitsMultiSelectCheckList to accept more than 1 habit

--- pillpall-mobile/src/components/HabitsMultiSelectCheckList/index.tsx
+++ pillpall-mobile/src/components/HabitsMultiSelectCheckList/index.tsx
@@ -6,18 +6,24 @@ import styles from './styles';
 
 interface HabitsMultiSelectChecklistProps {
   selectedOptions: string[];
-  onSelectOption: (option: string) => void;
+  onSelectOption: (options: string[]) => void;
 }
 
 const HabitsMultiSelectChecklist: FC<HabitsMultiSelectChecklistProps> = (props) => {
   const [expanded, setExpanded] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState(props.selectedOptions);
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
 
   const handleSelectOption = (option: string) => {
-    props.onSelectOption(option);
+    const updatedOptions = selectedOptions.includes(option)
+      ? selectedOptions.filter((item) => item !== option)
+      : [...selectedOptions, option];
+
+    setSelectedOptions(updatedOptions);
+    props.onSelectOption(updatedOptions);
   };
 
   return (
@@ -25,31 +31,31 @@ const HabitsMultiSelectChecklist: FC<HabitsMultiSelectChecklistProps> = (props) 
         <Text style={appStyles.body1}>Life Style Habits</Text>
 
         <TouchableOpacity style={styles.header} onPress={toggleExpanded}>
-            <Text style={styles.headerText}>{`Selected: ${props.selectedOptions.length}`}</Text>
+            <Text style={styles.headerText}>{`Selected: ${selectedOptions.length}`}</Text>
             <Text>{expanded ? '-' : '+'}</Text>
         </TouchableOpacity>
         {expanded && (
         <View style={styles.optionsContainer}>
             <TouchableOpacity
-                style={[styles.option, props.selectedOptions.includes('Alcohol Consumption') && styles.selectedOption]}
+                style={[styles.option, selectedOptions.includes('Alcohol Consumption') && styles.selectedOption]}
                 onPress={() => handleSelectOption('Alcohol Consumption')}
             >
                 <Text>Alcohol Consumption</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                style={[styles.option, props.selectedOptions.includes('Diet') && styles.selectedOption]}
+                style={[styles.option, selectedOptions.includes('Diet') && styles.selectedOption]}
                 onPress={() => handleSelectOption('Diet')}
             >
                 <Text>Diet</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                style={[styles.option, props.selectedOptions.includes('Exercise') && styles.selectedOption]}
+                style={[styles.option, selectedOptions.includes('Exercise') && styles.selectedOption]}
                 onPress={() => handleSelectOption('Exercise')}
             >
                 <Text>Exercise</Text>
             </TouchableOpacity>
             <TouchableOpacity
-                style={[styles.option, props.selectedOptions.includes('Smoking') && styles.selectedOption]}
+                style={[styles.option, selectedOptions.includes('Smoking') && styles.selectedOption]}
                 onPress={() => handleSelectOption('Smoking')}
             >
                 <Text>Smoking</Text>
