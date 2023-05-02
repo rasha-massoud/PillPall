ea23ef77ef52186ef77d6b29d46fdce6cdd2ab9f
RashaM <rashamassoud1998@gmail.com>
2023-05-02 18:08:18 +0300

    Frontend: Updated the AddImage Component

--- pillpall-mobile/src/components/AddImage/index.tsx
+++ pillpall-mobile/src/components/AddImage/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useState } from 'react';
 import { View, TouchableOpacity, Image, Text } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { ImagePickerResult } from 'expo-image-picker/build/ImagePicker.types';
@@ -7,53 +7,54 @@ import { Ionicons } from '@expo/vector-icons';
 import styles from './styles';
 
 type AddImageProps = {
-    onImageSelected: (image: string | null) => void;
+  onImageSelected: (imageFile: File | null) => void;
 };
 
 const AddImage: FC<AddImageProps> = ({ onImageSelected }) => {
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
-    const pickImage = async () => {
-        let result: ImagePickerResult = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-        });
-    
-        console.log(result);
-    
-        if (!result.canceled && result.assets && result.assets.length > 0) {
-            setSelectedImage(result.assets[0].uri);
-            onImageSelected(result.assets[0].uri);
-        }
-    };
+  const pickImage = async () => {
+    let result: ImagePickerResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
 
-    const clearImage = () => {
-        setSelectedImage(null);
-        onImageSelected(null);
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      setSelectedImage(result.assets[0].uri);
+      const response = await fetch(result.assets[0].uri);
+      const blob = await response.blob();
+      const file = new File([blob], result.assets[0].uri);
+      onImageSelected(file);
     }
+  };
 
-    return (
-        <View style={styles.container}>
-            {selectedImage ? (
-                <>
-                    <TouchableOpacity onPress={pickImage} style={styles.changeImage}>
-                    <Ionicons name="camera-outline" size={24} color="#fff" />
-                    <Text style={styles.changeImageText}>Change Image</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={pickImage}>
-                    <Image source={{ uri: selectedImage }} style={styles.image} />
-                    </TouchableOpacity>
-                </>
-            ) : (
-                <TouchableOpacity onPress={pickImage} style={styles.addImage}>
-                    <Ionicons name="add-outline" size={24} color="#fff" />
-                    <Text style={styles.addImageText}>Add Image</Text>
-                </TouchableOpacity>
-            )}
-        </View>
-    );
+  const clearImage = () => {
+    setSelectedImage(null);
+    onImageSelected(null);
+  };
+
+  return (
+    <View style={styles.container}>
+      {selectedImage ? (
+        <>
+          <TouchableOpacity onPress={pickImage} style={styles.changeImage}>
+            <Ionicons name="camera-outline" size={24} color="#fff" />
+            <Text style={styles.changeImageText}>Change Image</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={pickImage}>
+            <Image source={{ uri: selectedImage }} style={styles.image} />
+          </TouchableOpacity>
+        </>
+      ) : (
+        <TouchableOpacity onPress={pickImage} style={styles.addImage}>
+          <Ionicons name="add-outline" size={24} color="#fff" />
+          <Text style={styles.addImageText}>Add Image</Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
 };
 
 export default AddImage;
