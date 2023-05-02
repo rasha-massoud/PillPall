3365660af536bf9fac8fde3a10c8212d30cfb79a
RashaM <rashamassoud1998@gmail.com>
2023-05-02 22:06:09 +0300

    Frontend: Updated the App.tsx for testing purposes

--- pillpall-mobile/App.tsx
+++ pillpall-mobile/App.tsx
@@ -14,6 +14,7 @@ import MedicationsAndHabits from './src/screens/medicationshabits';
 import ForgotPassword from './src/screens/forgotpassword';
 import ResetPassword from './src/screens/resetpassword';
 import Report from './src/screens/report';
+import EditReport from './src/screens/editreport';
 import ChangePassword from './src/screens/changepassword';
 import OnboardingStack from './src/navigation/AuthNavigator';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -41,7 +42,7 @@ export default function App() {
     <View style={styles.container}>
       <Provider store={store}>
         <NavigationContainer>
-          <Report/>
+          <EditReport/>
         </NavigationContainer>
       </Provider>
     </View>
