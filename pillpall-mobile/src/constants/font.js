dc5e155b5e657253aeb0f8d7f34fcd2a8397ab04
RashaM <rashamassoud1998@gmail.com>
2023-05-04 00:17:00 +0300

    Frontend: Updated the font.js file

--- pillpall-mobile/src/constants/font.js
+++ pillpall-mobile/src/constants/font.js
@@ -1,23 +1,22 @@
 import { useFonts, Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import * as Font from 'expo-font';
 
-export const fonts = {
-  regular: 'Roboto_400Regular',
-  medium: {
-    fontFamily: 'Roboto_400Regular',
-    fontWeight: 600,  
-  },
-  bold: {
-    fontFamily: 'Roboto_400Regular',
-    fontWeight: 800,
-  },
+const loadFonts = async () => {
+  await Font.loadAsync({
+    'open-sans-italic': require('./assets/fonts/OpenSans-Italic.ttf'),
+    'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans-medium': require('./assets/fonts/OpenSans-Medium.ttf'),
+  });
 };
 
-export const loadFonts = async () => {
-  await Font.loadAsync({
-    Roboto_400Regular,
-    Roboto_500Medium,
-  });
+loadFonts();
+
+export const fonts = {
+  regular: 'open-sans-regular',
+  medium: 'open-sans-medium',
+  bold: 'open-sans-bold',
+  italic: 'open-sans-italic',
 };
 
 export default {
