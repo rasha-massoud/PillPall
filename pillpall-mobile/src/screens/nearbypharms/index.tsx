bdb8df84352b144e5c7f842bfa7f016299604d1d
RashaM <rashamassoud1998@gmail.com>
2023-05-04 00:03:11 +0300

    Frontend: Updated the Nearby Pharms Screen

--- pillpall-mobile/src/screens/nearbypharms/index.tsx
+++ pillpall-mobile/src/screens/nearbypharms/index.tsx
@@ -2,8 +2,9 @@ import React, { FC, useState } from 'react'
 import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import NavBar from '../../components/NavBar';
 import styles from './styles';
-import HERE_API_KEY from '../../../config';
+import Config from 'react-native-config';
 
+const hereApiKey = Config.HERE_API_KEY;
 import Map from '../../components/Map'
 
 const NearbyPharms: FC = () => {
@@ -19,7 +20,7 @@ const NearbyPharms: FC = () => {
                 style={styles.image}
             />
 
-            <Map apiKey={HERE_API_KEY || ''} />
+            <Map apiKey={hereApiKey || ''} />
 
         </SafeAreaView>
     );
