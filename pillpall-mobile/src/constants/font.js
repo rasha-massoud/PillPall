127e62a9a8ea428d51d0aa83e28c196b30d693a9
RashaM <rashamassoud1998@gmail.com>
2023-05-04 00:35:36 +0300

    Frontend: Updated the font.js file

--- pillpall-mobile/src/constants/font.js
+++ pillpall-mobile/src/constants/font.js
@@ -2,10 +2,10 @@ import * as Font from 'expo-font';
 
 const loadFonts = async () => {
   await Font.loadAsync({
-    'open-sans-italic': require('./assets/fonts/OpenSans-Italic.ttf'),
-    'open-sans-regular': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-    'open-sans-medium': require('./assets/fonts/OpenSans-Medium.ttf'),
+    'open-sans-italic': require('../../assets/fonts/OpenSans-Italic.ttf'),
+    'open-sans-regular': require('../../assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('../../assets/fonts/OpenSans-Bold.ttf'),
+    'open-sans-medium': require('../../assets/fonts/OpenSans-Medium.ttf'),
   });
 };
 
