64ac1217bd2e8821588e1e88eaba00d044aa9308
RashaM <rashamassoud1998@gmail.com>
2023-05-04 02:15:45 +0300

    Frontend: Updated the appStyles.js file

--- pillpall-mobile/src/constants/appStyles.js
+++ pillpall-mobile/src/constants/appStyles.js
@@ -3,41 +3,37 @@ import { fonts } from './font';
 
 const appStyles = StyleSheet.create({
   mainTitle: {
-    fontFamily: fonts.medium.fontFamily,
-    fontWeight: fonts.medium.fontWeight,
+    ...fonts.bold,
     fontSize: 34,
     letterSpacing: 0.73, 
   },
   pageTitle: {
-    fontFamily: fonts.medium.fontFamily,
-    fontWeight: fonts.medium.fontWeight,
+    ...fonts.medium,
     fontSize: 24,
     letterSpacing: 0, 
   },
   subTitle: {
-    fontFamily: fonts.medium.fontFamily,
-    fontWeight: fonts.medium.fontWeight,
+    ...fonts.medium,
     fontSize: 20,
     letterSpacing: 0.75, 
   },
   subTitle2: {
-    fontFamily: fonts.regular,
+    ...fonts.regular,
     fontSize: 16,
     letterSpacing: 0.93, 
   },  
   body1: {
-    fontFamily: fonts.regular,
+    ...fonts.regular,
     fontSize: 16,
     letterSpacing: 1, 
   },
   body2: {
-    fontFamily: fonts.regular,
+    ...fonts.regular,
     fontSize: 14,
     letterSpacing: 1.78, 
   },
   button: {
-    fontFamily: fonts.medium.fontFamily,
-    fontWeight: fonts.medium.fontWeight,
+    ...fonts.medium,
     fontSize: 14,
     letterSpacing: 1.5, 
   },
