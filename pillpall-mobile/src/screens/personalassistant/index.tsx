f5ced3cddddd1ae5c2e3921b2acda4ee246b25eb
RashaM <rashamassoud1998@gmail.com>
2023-05-03 23:19:21 +0300

    Frontend: Updated the Personal Assistant Screen

--- pillpall-mobile/src/screens/personalassistant/index.tsx
+++ pillpall-mobile/src/screens/personalassistant/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { SafeAreaView, Image, Text, Alert } from 'react-native';
 import NavBar from '../../components/NavBar';
 import axios from 'axios';
@@ -14,11 +14,11 @@ import styles from './styles';
 type QuestionType = 'question' | 'replacement' | 'effect' | 'instruction';
 
 const PersonalAssistant: FC = () => {
+
   const [selectedQuestionType, setSelectedQuestionType] = useState<QuestionType | null>(null);
   const [question, setQuestion] = useState<string>('');
   const [answer, setAnswer] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [showPopUp, setShowPopUp] = useState<boolean>(false);
 
   const handleSelectQuestionType = (questionType: QuestionType) => {
     setSelectedQuestionType(questionType);
@@ -30,148 +30,58 @@ const PersonalAssistant: FC = () => {
 
   const handleSendPress = async () => {
     if (!selectedQuestionType || !question) return;
-
     setIsLoading(true);
-
+  
     const token = await AsyncStorage.getItem('token');
-
     const data = new FormData();
     data.append('prompt', question);
-
-    if (question == 'replacement'){
-        const endpoint = 'chatbot/replacement';
-        await axios.post(`${API_URL}${endpoint}`, data, {
-            headers: {
-              'Accept': 'application/json',
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': "multipart/form-data",
-            },
-        })
-        .then((response) => {
-            setAnswer(response.data);
-            if (response.data.status == 'success'){
-                Alert.alert(
-                    'Success',
-                    'Answer Retrived Successfully.',
-                    [
-                      { text: 'OK' }
-                    ],
-                    { cancelable: false }
-                );
-            }
-        })
-        .catch((error) => {
-            setIsLoading(false);
-            Alert.alert(
-                'Fails',
-                'Personal Assistance Fails.',
-                [
-                    { text: 'OK' }
-                ],
-                { cancelable: false }
-            );
-        });
-    } else if (question == 'effect'){
-        const endpoint = 'chatbot/effect';
-        await axios.post(`${API_URL}${endpoint}`, data, {
-            headers: {
-              'Accept': 'application/json',
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': "multipart/form-data",
-            },
-        })
-        .then((response) => {
-            setAnswer(response.data);
-            if (response.data.status == 'success'){
-                Alert.alert(
-                    'Success',
-                    'Answer Retrived Successfully.',
-                    [
-                      { text: 'OK' }
-                    ],
-                    { cancelable: false }
-                );
-            }
-        })
-        .catch((error) => {
-            setIsLoading(false);
-            Alert.alert(
-                'Fails',
-                'Personal Assistance Fails.',
-                [
-                    { text: 'OK' }
-                ],
-                { cancelable: false }
-            );
-        });
-    } else if (question == 'instruction'){
-        const endpoint = 'chatbot/instruction';
-        await axios.post(`${API_URL}${endpoint}`, data, {
-            headers: {
-              'Accept': 'application/json',
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': "multipart/form-data",
-            },
-        })
-        .then((response) => {
-            setAnswer(response.data);
-            if (response.data.status == 'success'){
-                Alert.alert(
-                    'Success',
-                    'Answer Retrived Successfully.',
-                    [
-                      { text: 'OK' }
-                    ],
-                    { cancelable: false }
-                );
-            }
-        })
-        .catch((error) => {
-            setIsLoading(false);
-            Alert.alert(
-                'Fails',
-                'Personal Assistance Fails.',
-                [
-                    { text: 'OK' }
-                ],
-                { cancelable: false }
-            );
-        });
-    } else {
-        const endpoint = 'chatbot/question';
-        await axios.post(`${API_URL}${endpoint}`, data, {
-            headers: {
-              'Accept': 'application/json',
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': "multipart/form-data",
-            },
-        })
-        .then((response) => {
-            setAnswer(response.data);
-            if (response.data.status == 'success'){
-                Alert.alert(
-                    'Success',
-                    'Answer Retrived Successfully.',
-                    [
-                      { text: 'OK' }
-                    ],
-                    { cancelable: false }
-                );
-            }
-        })
-        .catch((error) => {
-            setIsLoading(false);
-            Alert.alert(
-                'Fails',
-                'Personal Assistance Fails.',
-                [
-                    { text: 'OK' }
-                ],
-                { cancelable: false }
-            );
-        });
+  
+    let endpoint = '';
+  
+    switch (question) {
+      case 'replacement':
+        endpoint = 'chatbot/replacement';
+        break;
+      case 'effect':
+        endpoint = 'chatbot/effect';
+        break;
+      case 'instruction':
+        endpoint = 'chatbot/instruction';
+        break;
+      default:
+        endpoint = 'chatbot/question';
+    }
+  
+    try {
+      const response = await axios.post(`${API_URL}${endpoint}`, data, {
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+  
+      setIsLoading(false);
+      setAnswer(response.data.answer.match(/^.+?\.(?!.*\.) /s)[0]);
+      if (response.data.status === 'success') {
+        Alert.alert(
+          'Success',
+          'Answer Retrived Successfully.',
+          [{ text: 'OK' }],
+          { cancelable: false },
+        );
+      }
+    } catch (error) {
+      setIsLoading(false);
+      Alert.alert(
+        'Fails',
+        'Personal Assistance Fails.',
+        [{ text: 'OK' }],
+        { cancelable: false },
+      );
     }
   };
+  
 
   return (
     <SafeAreaView style={styles.container}>
@@ -194,6 +104,8 @@ const PersonalAssistant: FC = () => {
       ) : (
         <>
           {answer && <DisplayData title="Answer" value={answer} />}
+          <Text>{answer}</Text>
+
           <CustomButton
             containerStyle={{ alignSelf: 'center' }}
             buttonprops={{ title: 'Send', onPress: handleSendPress }}
