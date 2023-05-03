329de25e10294caaa2a7e1f8dde3827893842ef8
RashaM <rashamassoud1998@gmail.com>
2023-05-04 01:17:15 +0300

    Frontend: Modified the PatientButtonTab file

--- pillpall-mobile/src/navigation/PatientButtonTab.tsx
+++ pillpall-mobile/src/navigation/PatientButtonTab.tsx
@@ -1,25 +1,9 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Image } from "react-native";
 
-import Welcome from "../screens/welcome";
-import ContactInfo from "../screens/contactinfo";
-import AnthropometricMeasurements from "../screens/measurements";
-import EmergencyContact from "../screens/emergencycontact";
-import VitalSigns from "../screens/vitalsigns";
-import MedicalHistory from "../screens/medicalhistory";
-import MedicationsAndHabits from "../screens/medicationshabits";
 import Report from "../screens/report";
-// import EditReport from "../screens/editreport"
 import MedicationSchedule from "../screens/medicationschedule";
-import AddMedicine from "../screens/addmedicine";
-import DeleteMedicine from "../screens/deletemedicine";
-import NearbyPharms from "../screens/nearbypharms";
-import BudgetTracker from "../screens/budgettracker";
 import PersonalAssistant from "../screens/personalassistant";
-import FileNum from "../screens/filenumbers";
-import AddFileNumber from "../screens/addfilenumber";
-import MedicalResults from "../screens/medicalresults";
-import AddMedicalResult from "../screens/addmedicalresult";
 import PatientSearch from "../screens/patientseach";
 import ChangePassword from "../screens/changepassword";
 
@@ -32,7 +16,7 @@ const PatientTabStack = () => {
             tabBarIcon: () => (
                 <Image
                     source={require('../../assets/navReportPatient.png')}
-                    // style={{ width: 20, height: 20 }}
+                    style={{ width: 55, height: 52 }}
                 />
             ),
             }}
@@ -44,7 +28,7 @@ const PatientTabStack = () => {
                 tabBarIcon: () => (
                     <Image
                         source={require('../../assets/navMedSchedule.png')}
-                        // style={{ width: 20, height: 20 }}
+                        style={{ width: 55, height: 52 }}
                     />
                 ),
                 }}
@@ -56,19 +40,31 @@ const PatientTabStack = () => {
             tabBarIcon: () => (
                 <Image
                     source={require('../../assets/navSearchDoc.png')}
-                    // style={{ width: 20, height: 20 }}
+                    style={{ width: 55, height: 52 }}
                 />
             ),
             }}
             name="Search"
             component={PatientSearch}
         />
+        {/* <Tabs.Screen
+            options={{
+            tabBarIcon: () => (
+                <Image
+                    source={require('../../assets/navIot.png')}
+                    style={{ width: 55, height: 52 }}
+                />
+            ),
+            }}
+            name="IoT"
+            component={Report}                   
+        /> */}
         <Tabs.Screen
             options={{
                 tabBarIcon: () => (
                 <Image
                     source={require('../assets/navRobot.png')}
-                    // style={{ width: 20, height: 20 }}
+                    style={{ width: 55, height: 52 }}
                 />
                 ),
             }}
@@ -80,7 +76,7 @@ const PatientTabStack = () => {
                 tabBarIcon: () => (
                 <Image
                     source={require('../assets/navSettings.png')}
-                    // style={{ width: 20, height: 20 }}
+                    style={{ width: 55, height: 52 }}
                 />
                 ),
             }}
