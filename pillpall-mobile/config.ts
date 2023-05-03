b57e9bed5b01806341458d9bb365459bf5de31d0
RashaM <rashamassoud1998@gmail.com>
2023-05-04 00:01:22 +0300

    Frontend: Updated the config file to include the fonts from the assets

--- pillpall-mobile/config.ts
+++ pillpall-mobile/config.ts
@@ -2,4 +2,10 @@ import Config from 'react-native-config';
 
 const HERE_API_KEY = Config.HERE_API_KEY;
 
-export default HERE_API_KEY;
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+};
