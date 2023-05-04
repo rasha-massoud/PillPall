7debe2db8a444f88a1872e249a5b3b938c8b7971
RashaM <rashamassoud1998@gmail.com>
2023-05-04 20:21:02 +0300

    Frontend: Added the navigation to the Welcome Screen

--- pillpall-mobile/src/screens/welcome/index.tsx
+++ pillpall-mobile/src/screens/welcome/index.tsx
@@ -1,16 +1,19 @@
 import React, { FC } from 'react'
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView, Image } from 'react-native';
 import CustomButton from '../../components/CustomButton';
 import TextTitle from '../../components/TextTitle';
 import SubTitleText from '../../components/SubTitleText';
 import Body1Text from '../../components/Body1Text';
 import { colors } from '../../constants/palette'
+import { useNavigation } from '@react-navigation/core';
 import styles from './styles'
 
 const Welcome: FC = () => {
 
-    const handleLoginPress = () => {
-        // Navigate to Login in screen
+    const navigation = useNavigation();
+
+    const handleCancelPress = () => {
+        navigation.navigate("ContactInfo" as never, {} as never);
     };
 
     return (
@@ -27,7 +30,7 @@ const Welcome: FC = () => {
         <Body1Text context="As a patient, your health information is critical to help us understand your medical needs and provide the best care possible. To ensure we have a comprehensive understanding of your health status, we have a six-step process that will help us gather all the necessary information. Each step is designed to collect different aspects of your health information.
 By completing each step, we can provide you with personalized and effective care. Let's walk through each step together."></Body1Text>
 
-        <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: () => console.log('Continue') }}  />
+        <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: handleCancelPress }}  />
 
     </SafeAreaView>
   );
