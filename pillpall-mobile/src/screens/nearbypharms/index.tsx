6c2a7ced2e611bb866e8efb97c57ebd5df8d7a26
RashaM <rashamassoud1998@gmail.com>
2023-05-05 00:08:26 +0300

    Frontend: Updated the navigation of the Nearby Pharms Screen

--- pillpall-mobile/src/screens/nearbypharms/index.tsx
+++ pillpall-mobile/src/screens/nearbypharms/index.tsx
@@ -1,23 +1,28 @@
 import React, { FC, useState } from 'react'
-import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
-import NavBar from '../../components/NavBar';
+import { SafeAreaView } from 'react-native';
+import NavBar1 from '../../components/NavBar1';
 import styles from './styles';
 import Config from 'react-native-config';
+import { useNavigation } from '@react-navigation/core';
+
+import Map from '../../components/Map'
 
 const hereApiKey = Config.HERE_API_KEY;
-import Map from '../../components/Map'
 
 const NearbyPharms: FC = () => {
     
+    const navigation = useNavigation();
+
+    const handleBackPress = () => {
+        navigation.navigate("MedicationSchedule" as never, {} as never);
+    }
+
     return (
     
         <SafeAreaView style={styles.container}>
-            <NavBar
+            <NavBar1
                 title="Nearby Pharmacies"
-            />
-            <Image
-                source={require('../../../assets/nearbyPharm.png')}
-                style={styles.image}
+                image1={{ source: require('../../../assets/back.png'), onPress: handleBackPress }}
             />
 
             <Map apiKey={hereApiKey || ''} />
