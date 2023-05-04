7b1c3068917b0cc5fe0bbbd6bb9a9dfc2dd28a75
RashaM <rashamassoud1998@gmail.com>
2023-05-04 23:41:42 +0300

    Frontend: Created a NavBar4 Component

---
+++ pillpall-mobile/src/components/NavBar4/index.tsx
@@ -0,0 +1,40 @@
+import React, { FC } from 'react'
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import PageTitle from '../PageTitle';
+
+import styles from './styles';
+
+interface NavBar4Props {
+    title: string;
+    image1: { source: any, onPress: () => void };
+    image2: { source: any, onPress: () => void };
+    image3: { source: any, onPress: () => void };
+    image4: { source: any, onPress: () => void };
+}
+
+const NavBar4: FC<NavBar4Props> = (props) => {
+
+  return (
+  
+    <View style={styles.container}>
+        <PageTitle title={props.title} />
+        <View style={styles.imagesContainer}>
+            <TouchableOpacity onPress={props.image1.onPress} style={styles.imageContainer}>
+                <Image source={props.image1.source} style={styles.image} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={props.image2.onPress} style={styles.imageContainer}>
+                <Image source={props.image2.source} style={styles.image} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={props.image3.onPress} style={styles.imageContainer}>
+                <Image source={props.image3.source} style={styles.image} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={props.image3.onPress} style={styles.imageContainer}>
+                <Image source={props.image4.source} style={styles.image} />
+            </TouchableOpacity>
+        </View>
+    </View>
+
+  );
+};
+
+export default NavBar4;
---
+++ pillpall-mobile/src/components/NavBar4/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import { ViewStyle, TextStyle, ImageStyle } from 'react-native';
+import { colors } from '../../constants/palette';
+
+interface Styles {
+    container: ViewStyle;
+    imagesContainer: ViewStyle;
+    imageContainer: ViewStyle;
+    image: ImageStyle;
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    imagesContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: 92,
+    },
+    imageContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 30,
+      height: 30,
+    },
+    image: {
+      width: 25,
+      height: 25,
+    },
+});
+  
+export default styles;
