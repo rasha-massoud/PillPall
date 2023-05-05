580b6d7b862566cabf6010183e98d991a137e482
RashaM <rashamassoud1998@gmail.com>
2023-05-05 11:26:23 +0300

    Backend: Defined the CheckDoctorRole in the Kernel.php file

--- pillpall-backend/app/Http/Kernel.php
+++ pillpall-backend/app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel{
     protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'check.patient.role' => \App\Http\Middleware\CheckPatientRole::class,
+        'check.doctor.role' => \App\Http\Middleware\CheckDoctorRole::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
