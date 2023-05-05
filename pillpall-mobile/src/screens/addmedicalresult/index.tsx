8e4d4db8314fed8ed11b4df4282d1ed4ad0f53cb
RashaM <rashamassoud1998@gmail.com>
2023-05-06 02:22:24 +0300

    Frontend: Linked the Add Medical Result Screen to the BE

--- pillpall-mobile/src/screens/addmedicalresult/index.tsx
+++ pillpall-mobile/src/screens/addmedicalresult/index.tsx
@@ -6,6 +6,8 @@ import TwoCustomButton from '../../components/TwoCustomButton';
 import TextInputwithLabel from '../../components/TextInputwithLabel';
 import AddDocument from '../../components/AddDocument';
 import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import API_URL from '../../constants/url';
 
 import styles from './styles';
 
@@ -37,15 +39,51 @@ const AddMedicalResult: FC = () => {
         }
         
         try {
-            const formData = new FormData();
-            formData.append('file', file);
-            formData.append('testingDate', testingDate);
-            formData.append('fileName', fileName);
-            formData.append('description', description);
+            const data = new FormData();
+            data.append('file', file);
+            data.append('testing_date', testingDate);
+            data.append('fileName', file);
+            data.append('description', description);
         
-            // const response = await axios.post('https://example.com/api/add-medical-result', formData);
-        
-            // console.log(response.data);
+            const token = await AsyncStorage.getItem('token');
+
+            const endpoint= 'med/add_medical_result';
+            await axios.post(`${API_URL}${endpoint}`, data, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': "multipart/form-data",
+                },
+            })
+            .then((response) => {
+                console.log(response.data);
+                if(response.data.status == 'success'){
+                    Alert.alert(
+                        'Success',
+                        'The medical result is successfully added.',
+                        [
+                            { 
+                                text: 'OK',
+                                onPress: () => {
+                                    navigation.navigate("FileNum" as never, {} as never);
+                                },
+                            }
+                        ],
+                        { cancelable: false }
+                    );
+                    navigation.navigate("Profile" as never, {} as never);
+                }
+                else{
+                    Alert.alert(
+                        'Fails',
+                        'Request Fails.',
+                        [
+                            { text: 'OK' }
+                        ],
+                        { cancelable: false }
+                    );
+                }
+            })
         } catch (error) {
             console.error(error);
         }
@@ -72,7 +110,7 @@ const AddMedicalResult: FC = () => {
 
 
     const handleDocumentSelected = (document: File) => {
-      setFile(document);
+        setFile(document);
     };
 
     const handleBackPress = () => {
