8e8073a06f2fba6d12085d150fc65fc97efec52b
RashaM <rashamassoud1998@gmail.com>
2023-05-05 11:25:21 +0300

    Backend: Added the CheckPatientRole Middleware

--- pillpall-backend/app/Http/Middleware/CheckDoctorRole.php
+++ pillpall-backend/app/Http/Middleware/CheckDoctorRole.php
@@ -5,16 +5,16 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
-class CheckDoctorRole
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
+class CheckDoctorRole{
+    
+    public function handle(Request $request, Closure $next): Response{
+        
+        if (Auth::check() && Auth::user()->role === 'doctor') {
+            return $next($request);
+        }
+    
+        return response()->json(['error' => 'Unauthorized'], 401);
     }
 }
