18dac174d782dcc41909db3d76872eb2fd84db35
RashaM <rashamassoud1998@gmail.com>
2023-05-07 02:22:50 +0300

    Frontend: Modified the Medical Result Screen

--- pillpall-mobile/src/screens/medicalresults/index.tsx
+++ pillpall-mobile/src/screens/medicalresults/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
-import { View, Text, SafeAreaView, FlatList, Alert } from 'react-native';
+import { View, Text, SafeAreaView, FlatList, Alert, TouchableOpacity, Linking  } from 'react-native';
 import axios from 'axios';
 import CustomButton from '../../components/CustomButton';
 import MedicalResultCard from '../../components/MedicalResultCard';
@@ -17,6 +17,7 @@ interface MedicalResult {
     uri: string;
     testing_date: string;
     description: string;
+    file_name: string;
 }
 
 const MedicalResults: FC = () => {
@@ -28,51 +29,77 @@ const MedicalResults: FC = () => {
   const [isSuccess, setIsSuccess] = useState<boolean>(true);
 
   useEffect(() => {
-
     const fetchData = async () => {
       setIsLoading(true);
-
+  
       const token = await AsyncStorage.getItem('token');
   
       const endpoint = 'med/get_medical_results';
   
-      await axios.get(`${API_URL}${endpoint}`, {
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': "multipart/form-data",
-        },
-      })
-      .then((response) => {
-        setResults(response.data.results);
-        setIsLoading(false);
-        if (response.data.status !== "success"){
-          setIsLoading(true);
-          Alert.alert(
-            'Failure',
-            'Request Fails.',
-            [
-              { text: 'OK' }
-            ],
-            { cancelable: false }
-          );
-        }
-      })
-      .catch((error) => {
+      await axios
+        .get(`${API_URL}${endpoint}`, {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          setIsLoading(false);
+          if (response.data.status === 'success') {
+            const formattedResults = response.data.results.map((result: MedicalResult) => ({
+              ...result,
+              uri: `${API_URL}${endpoint}/${result.file_name}?id=${result.id}`,
+            }));
+            setResults(formattedResults);
+            setIsSuccess(true);
+          } else {
+            setIsSuccess(false);
+            setIsLoading(true);
+            Alert.alert(
+              'Failure',
+              'Request Fails.',
+              [{ text: 'OK' }],
+              { cancelable: false }
+            );
+          }
+        })
+        .catch((error) => {
           console.error('An error occurred while getting the medical results');
-      });
+        });
     };
   
     fetchData();
   }, []);
+  
+  
+
+  const handleOpenFile = (uri: string) => {
+    Linking.canOpenURL(uri)
+      .then((supported) => {
+        if (supported) {
+          Linking.openURL(uri);
+        } else {
+          console.error('Cannot open URL:', uri);
+        }
+      })
+      .catch((error) => {
+        console.error('An error occurred while opening the file:', error);
+      });
+  };
+  
 
   const renderItem = ({ item }: { item: MedicalResult }) => (
-    <MedicalResultCard
-      file={item}
-      testingDate={item.testing_date}
-      fileName={item.name}
-      description={item.description}
-    />
+    <TouchableOpacity
+      style={styles.cardContainer}
+      onPress={() => handleOpenFile(item.uri)}
+    >
+      <View>
+        <Text style={styles.testingDate}>{item.testing_date}</Text>
+        <Text style={styles.description}>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
   );
 
   const handleFileNumberPress = () => {
@@ -91,42 +118,46 @@ const MedicalResults: FC = () => {
     navigation.navigate("AddMedicalResult" as never, {} as never);
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <NavBar3
-        title="Medical Results"
-        image1={{ source: require('../../../assets/filenumber.png'), onPress: handleFileNumberPress }}
-        image2={{ source: require('../../../assets/results.png'), onPress: handleResultPress }}
-        image3={{ source: require('../../../assets/searchdoc.png'), onPress: handleSearchDoctorPress }}
+return (
+  <SafeAreaView style={styles.container}>
+    <NavBar3
+      title="Medical Results"
+      image1={{ source: require('../../../assets/filenumber.png'), onPress: handleFileNumberPress }}
+      image2={{ source: require('../../../assets/results.png'), onPress: handleResultPress }}
+      image3={{ source: require('../../../assets/searchdoc.png'), onPress: handleSearchDoctorPress }}
     />
 
-      {results.length === 0 && isSuccess && (
-        <View style={styles.noDataContainer}>
-          <View>
-            <Text style={styles.noDataText}>There is no data yet.</Text>
-          </View>
-          <CustomButton containerStyle={{ alignSelf: 'center' }} buttonprops={{ title: "ADD", onPress: handleAddPress }}  />
+    {results.length === 0 && isSuccess && (
+      <View style={styles.noDataContainer}>
+        <View>
+          <Text style={styles.noDataText}>There is no data yet.</Text>
         </View>
-      )}
+        <CustomButton
+          containerStyle={{ alignSelf: 'center' }}
+          buttonprops={{ title: "ADD", onPress: handleAddPress }}
+        />
+      </View>
+    )}
 
-  
-        {results.length !== 0 && isSuccess && (
-          <FlatList
-            data={results}
-            renderItem={renderItem}
-            keyExtractor={(item) => item.id.toString()}
-            contentContainerStyle={styles.resultsList}
-            showsVerticalScrollIndicator={true}
-            scrollEnabled={true}
-          />
-        )}
+    {results.length !== 0 && isSuccess && (
+      <>
+        <CustomButton
+          containerStyle={styles.btn}
+          buttonprops={{ title: "Add Medical Result", onPress: handleAddPress }}
+        />
 
-        {results.length !== 0 && isSuccess && (
-          <CustomButton containerStyle={{ alignSelf: 'center', width: 55, margin: 5, backgroundColor: colors.darker_gray }} buttonprops={{ title: "+", onPress: handleAddPress }}  />
-        )}
-
-    </SafeAreaView>
-  );
+        <FlatList
+          data={results}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={styles.resultsList}
+          showsVerticalScrollIndicator={true}
+          scrollEnabled={true}
+        />
+      </>
+    )}
+  </SafeAreaView>
+);
 };
 
 export default MedicalResults;
--- pillpall-mobile/src/screens/medicalresults/styles.ts
+++ pillpall-mobile/src/screens/medicalresults/styles.ts
@@ -26,6 +26,36 @@ const styles = StyleSheet.create({
     resultsList: {
       flexGrow: 1,
     },
+    btn: {
+      alignSelf: 'center',
+       width: '90%', 
+       marginTop: 30, 
+       marginBottom: 10, 
+       borderRadius:10,
+    },
+    cardContainer: {
+      backgroundColor: '#FFFFFF',
+      borderRadius: 8,
+      padding: 16,
+      marginBottom: 16,
+      shadowColor: '#000000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+      elevation: 2,
+    },
+    testingDate: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 8,
+    },
+    description: {
+      fontSize: 14,
+      color: '#888888',
+    },
 });
 
 export default styles;
