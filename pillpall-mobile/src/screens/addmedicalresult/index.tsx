d35a2f1758626f02684df82105271dbeb3316714
RashaM <rashamassoud1998@gmail.com>
2023-05-07 00:30:42 +0300

    Frontend: Updated the design and linked the Add Medical Result Screen to the BE

--- pillpall-mobile/src/screens/addmedicalresult/index.tsx
+++ pillpall-mobile/src/screens/addmedicalresult/index.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react';
-import { SafeAreaView, Alert } from 'react-native';
+import { SafeAreaView, Alert, View, Text, Image } from 'react-native';
 import NavBar1 from '../../components/NavBar1';
 import axios from 'axios';
 import TwoCustomButton from '../../components/TwoCustomButton';
@@ -8,23 +8,37 @@ import AddDocument from '../../components/AddDocument';
 import { useNavigation } from '@react-navigation/core';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import API_URL from '../../constants/url';
-
+import * as DocumentPicker from 'expo-document-picker';
+import DisplayData from '../../components/DisplayData';
 import styles from './styles';
+import CustomButton from '../../components/CustomButton';
 
 const AddMedicalResult: FC = () => {
   const navigation = useNavigation();
 
-  const [file, setFile] = useState<File | null>(null);
+  const [selectedFile, setSelectedFile] = useState<string | null>(null);
   const [testingDate, setTestingDate] = useState('');
-  const [fileName, setFileName] = useState('');
   const [description, setDescription] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [statusMessage, setStatusMessage] = useState('');
+
+  const selectFile = async () => {
+    try {
+      const result = await DocumentPicker.getDocumentAsync({
+        type: 'application/pdf',
+        copyToCacheDirectory: false,
+      });
+  
+      if (result.type === 'success') {
+        setSelectedFile(result.uri);
+      }
+    } catch (error) {
+      console.log('Error selecting file:', error);
+    }
+  };
 
   const handleTestingDateChange = (value: string) => {
     setTestingDate(value);
-  };
-
-  const handleFileNameChange = (value: string) => {
-    setFileName(value);
   };
 
   const handleDescriptionChange = (value: string) => {
@@ -32,57 +46,75 @@ const AddMedicalResult: FC = () => {
   };
 
   const handleAddPress = async () => {
-    if (!file) {
-      alert('Please select a file');
-      return;
-    }
-
-    try {
-      const data = new FormData();
-      data.append('file', file);
-      data.append('testing_date', testingDate);
-      data.append('description', description);
-
-      const token = await AsyncStorage.getItem('token');
-
-      console.log(data);
-      const endpoint = 'med/add_medical_result';
-      await axios.post(`${API_URL}${endpoint}`, data, {
-        headers: {
-          'Accept': 'application/json',
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'multipart/form-data',
-        },
-      }).then((response) => {
-        console.log(response.data);
-        if (response.data.status == 'success') {
-          Alert.alert(
-            'Success',
-            'The medical result is successfully added.',
-            [
-              {
-                text: 'OK',
-                onPress: () => {
-                  navigation.navigate('FileNum' as never, {} as never);
-                },
-              },
-            ],
-            { cancelable: false }
-          );
-          navigation.navigate('Profile' as never, {} as never);
-        } else {
-          Alert.alert(
+    if (!selectedFile && !testingDate && !description) {
+        Alert.alert(
             'Fails',
-            'Request Fails.',
+            'Missing Field. Please make sure to fill all fields.',
             [
               { text: 'OK' }
             ],
             { cancelable: false }
-          );
-        }
-      });
+        );
+        return;
+    }
+
+    try {
+        setLoading(true);
+
+        const fileUri = selectedFile;
+        const fileExtension = fileUri!.split('.').pop();
+
+        const data = new FormData();
+        data.append('file', {
+            uri: fileUri!,
+            name: `medical_result.${fileExtension}`,
+            type: 'application/pdf',
+        } as any);        
+        data.append('testing_date', testingDate);
+        data.append('description', description);
+
+        const token = await AsyncStorage.getItem('token');
+
+        console.log(data);
+        const endpoint = 'med/add_medical_result';
+        await axios.post(`${API_URL}${endpoint}`, data, {
+            headers: {
+            'Accept': 'application/json',
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'multipart/form-data',
+            },
+        }).then((response) => {
+            console.log(response.data);
+            if (response.data.status == 'success') {
+            Alert.alert(
+                'Success',
+                'The medical result is successfully added.',
+                [
+                {
+                    text: 'OK',
+                    onPress: () => {
+                    navigation.navigate('FileNum' as never, {} as never);
+                    },
+                },
+                ],
+                { cancelable: false }
+            );
+            navigation.navigate('Profile' as never, {} as never);
+            } else {
+            Alert.alert(
+                'Fails',
+                'Request Fails.',
+                    [
+                        { text: 'OK' }
+                    ],
+                { cancelable: false }
+            );
+            }
+        });
     } catch (error) {
-      console.error(error);
+        console.error(error);
+    } finally {
+        setLoading(false);
     }
   };
 
@@ -105,46 +137,51 @@ const AddMedicalResult: FC = () => {
     );
   };
 
-  const handleDocumentSelected = (selectedFile: File) => {
-    setFile(selectedFile);
-  };
-
   const handleBackPress = () => {
     navigation.navigate('MedicalResults' as never, {} as never);
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <NavBar1
-        title="Add Medical Results"
-        image1={{ source: require('../../../assets/back.png'), onPress: handleBackPress }}
-      />
-      <TextInputwithLabel
-        label='Testing Date'
-        placeholder="YYYY-MM-DD"
-        textinputprops={{ secureTextEntry: false }}
-        onChangeText={handleTestingDateChange}
-      />
-      <TextInputwithLabel
-        label='File Name'
-        placeholder='Enter the File Name'
-        textinputprops={{ secureTextEntry: false }}
-        onChangeText={handleFileNameChange}
-      />
-      <TextInputwithLabel
-        label='Description'
-        placeholder='Enter Description'
-        textinputprops={{ secureTextEntry: false }}
-        onChangeText={handleDescriptionChange}
-      />
-  
-      <AddDocument onDocumentSelected={handleDocumentSelected} />
-  
-      <TwoCustomButton
-        containerStyle={{ alignSelf: 'center' }}
-        buttonprops2={{ title: 'Cancel', onPress: handleCancelPress }}
-        buttonprops1={{ title: 'Add', onPress: handleAddPress }}
-      />
+        <NavBar1
+            title="Add Medical Results"
+            image1={{ source: require('../../../assets/back.png'), onPress: handleBackPress }}
+        />
+        
+        <Image
+            source={require('../../../assets/addresultscreen.png')}
+            style={styles.image}
+        />        
+        
+        <CustomButton containerStyle={ styles.btn} buttonprops={{ title: "Select File", onPress: selectFile }}  />
+
+        {selectedFile && (
+            <View style={styles.selectedFileContainer}>
+                <DisplayData title='File Selected' value='Success'/>
+            </View>
+        )}
+
+        <TextInputwithLabel
+            label='Testing Date'
+            placeholder="YYYY-MM-DD"
+            textinputprops={{ secureTextEntry: false }}
+            onChangeText={handleTestingDateChange}
+        />
+        <TextInputwithLabel
+            label='Description'
+            placeholder='Enter Description'
+            textinputprops={{ secureTextEntry: false }}
+            onChangeText={handleDescriptionChange}
+        />
+            
+        <TwoCustomButton
+            containerStyle={{ alignSelf: 'center' }}
+            buttonprops2={{ title: 'Cancel', onPress: handleCancelPress }}
+            buttonprops1={{ title: 'Add', onPress: handleAddPress }}
+        />
+
+        {statusMessage ? <Text style={styles.statusMessage}>{statusMessage}</Text> : null}
+
     </SafeAreaView>
   );
 }
--- pillpall-mobile/src/screens/addmedicalresult/styles.ts
+++ pillpall-mobile/src/screens/addmedicalresult/styles.ts
@@ -1,9 +1,17 @@
 import { StyleSheet } from 'react-native';
 import { ViewStyle, TextStyle, ImageStyle } from 'react-native';
 import { Dimensions } from 'react-native';
+import { colors } from '../../constants/palette';
+import appStyles from '../../constants/appStyles';
+import { black } from 'react-native-paper/lib/typescript/src/styles/themes/v2/colors';
 
 interface Styles {
     container: ViewStyle;
+    selectedFileContainer: ViewStyle;
+    selectedFileName: TextStyle;
+    statusMessage: TextStyle;
+    image: ImageStyle;
+    btn: TextStyle;
 }
 
 const { width, height } = Dimensions.get('window');
@@ -15,6 +23,35 @@ const styles = StyleSheet.create<Styles>({
         width: LOGIN_PAGE_WIDTH,
         marginHorizontal: width * 0.05, 
     },
+    selectedFileContainer: {
+        marginVertical: 16,
+    },
+    selectedFileName: {
+        fontSize: 16,
+        marginBottom: 8,
+    },
+    statusMessage: {
+        fontSize: 16,
+        marginTop: 16,
+        textAlign: 'center',
+    },
+    image: {
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        height: '35%',
+        width: '60%',
+    },
+    btn: {
+        alignSelf: 'center', 
+        width: '60%', 
+        backgroundColor: colors.dark_gray,
+        borderRadius: 100,
+        shadowColor: colors.darker_gray,
+        shadowRadius: 50,
+        marginBottom: 30,
+    }
 });
 
 export default styles;
