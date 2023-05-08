54e29d7087153aa45f194707afe5d549684a58da
RashaM <rashamassoud1998@gmail.com>
2023-05-08 04:01:47 +0300

    Frontend: Updated the PatientResult Screen

--- pillpall-mobile/src/screens/patientresult/index.tsx
+++ pillpall-mobile/src/screens/patientresult/index.tsx
@@ -1,51 +1,113 @@
 import React, { FC, useEffect, useState } from 'react';
-import { View, Text, SafeAreaView, FlatList } from 'react-native';
+import { View, Alert, SafeAreaView, FlatList, Linking, TouchableOpacity, Text } from 'react-native';
 import axios from 'axios';
 import NavBar from '../../components/NavBar';
 import MedicalResultCard from '../../components/MedicalResultCard';
+import { useNavigation } from '@react-navigation/core';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import API_URL from '../../constants/url';
 
 import styles from './styles';
+
+interface PatientReportProps {
+    route: {
+        params: {
+          patientId: number;
+        };
+    };
+  }
 
 interface PatientResultData {
     id: number;
     name: string;
     type: string;
     uri: string;
-    testingDate: string;
+    testing_date: string;
     description: string;
+    file_name: string;
 }
 
-const PatientResult: FC = () => {
+const PatientResult: FC<PatientReportProps> = ({route}) => {
+
+    const { patientId } = route.params;
+
+    const navigation = useNavigation();
+
     const [results, setResults] = useState([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [isSuccess, setIsSuccess] = useState<boolean>(true);
 
     useEffect(() => {
-        // axios
-        //   .get('*******************************************')
-        //   .then(response => {
-        //     setResults(response.data.results);
-        //   })
-        //   .catch(error => {
-        //     console.error(error);
-        //   });
-    }, []);
+        const fetchData = async () => {
+            setIsLoading(true);
+        
+            const token = await AsyncStorage.getItem('token');
+        
+            const endpoint = 'doctor/get_patient_results';
+        
+            await axios
+              .get(`${API_URL}${endpoint}/${patientId}`, {
+                headers: {
+                  Accept: 'application/json',
+                  Authorization: `Bearer ${token}`,
+                  'Content-Type': 'multipart/form-data',
+                },
+              })
+              .then((response) => {
+                console.log(response.data);
+                setIsLoading(false);
+                console.log(response.data);
+                if (response.data.status === 'success') {
+                  const formattedResults = response.data.results.map((result: PatientResultData) => ({
+                    ...result,
+                    uri: `http://192.168.0.100:8000/storage/storage/images/${result.file_name}`,
+                  }));
+                  setResults(formattedResults);
+                  setIsSuccess(true);
+                } else {
+                  setIsSuccess(false);
+                  setIsLoading(true);
+                  Alert.alert(
+                    'Failure',
+                    'Request Fails.',
+                    [{ text: 'OK' }],
+                    { cancelable: false }
+                  );
+                }
+              })
+              .catch((error) => {
+                console.error('An error occurred while getting the medical results');
+              });
+          };
+        
+          fetchData();
+        }, []);
 
-    const renderItem = ({ item }: { item: PatientResultData }) => (
-        <MedicalResultCard
-        file={item}
-        testingDate="April 30, 2023"
-        fileName={item.name}
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        />
-    );
+        const handleOpenFile = (uri: string) => {
+            Linking.canOpenURL(uri)
+              .then((supported) => {
+                if (supported) {
+                  Linking.openURL(uri);
+                } else {
+                  console.error('Cannot open URL:', uri);
+                }
+              })
+              .catch((error) => {
+                console.error('An error occurred while opening the file:', error);
+              });
+          };
 
-    function handleSearchDoctorPress(): void {
-    }
-
-    function handleFileNumberPress(): void {
-    }
-
-    function handleResultPress(): void {
-    }
+          const renderItem = ({ item }: { item: PatientResultData }) => (
+            <TouchableOpacity
+              style={styles.cardContainer}
+              onPress={() => handleOpenFile(item.uri)}
+            >
+              <View>
+                <Text style={styles.testingDate}>{item.testing_date}</Text>
+                <Text style={styles.description}>{item.description}</Text>
+              </View>
+            </TouchableOpacity>
+          );
 
     return (
         <SafeAreaView style={styles.container}>
--- pillpall-mobile/src/screens/patientresult/styles.ts
+++ pillpall-mobile/src/screens/patientresult/styles.ts
@@ -14,6 +14,29 @@ const styles = StyleSheet.create({
       flexGrow: 1,
       marginTop: 16,
     },
+    cardContainer: {
+      backgroundColor: '#FFFFFF',
+      borderRadius: 8,
+      padding: 16,
+      marginBottom: 16,
+      shadowColor: '#000000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.1,
+      shadowRadius: 4,
+      elevation: 2,
+    },
+    testingDate: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      marginBottom: 8,
+    },
+    description: {
+      fontSize: 14,
+      color: '#888888',
+    },
 });
 
 export default styles;
