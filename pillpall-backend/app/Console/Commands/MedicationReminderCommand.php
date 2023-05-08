3a7a13e2bdfaf4df51113006d64f52078ddc4c48
RashaM <rashamassoud1998@gmail.com>
2023-05-08 14:07:10 +0300

    Backend: Created the Medication Reminders Command

--- pillpall-backend/app/Console/Commands/MedicationReminderCommand.php
+++ pillpall-backend/app/Console/Commands/MedicationReminderCommand.php
@@ -4,27 +4,60 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class MedicationReminderCommand extends Command
-{
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
+use Exception;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+
+use App\Models\User;
+use App\Models\Medication;
+
+class MedicationReminderCommand extends Command{
+
     protected $signature = 'app:medication-reminder-command';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
+    protected $description = 'Check medication timings and send reminders to the Arduino';
 
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        //
+    public function handle(){
+        try {
+            $user = auth()->user();
+            $currentTime = Carbon::now();
+            $currentDayOfWeek = strtolower($currentTime->englishDayOfWeek);
+        
+            $isFirstOfMonth = $currentTime->day === 1;
+        
+            $medications = $user->medications()
+                ->where(function ($query) use ($currentDayOfWeek, $isFirstOfMonth) {
+                    $query->where('days', 'Everyday')
+                        ->orWhere('days', 'like', "%{$currentDayOfWeek}%");
+        
+                    if ($isFirstOfMonth) {
+                        $query->orWhere('first_of_each_month', true);
+                    }
+                })
+                ->get();
+        
+            if ($medications->isNotEmpty()) {
+                $this->sendCommandToArduino();
+        
+                $this->info('Medication reminder sent to the Arduino!');
+            } else {
+                $this->info('No medications to remind at the current time.');
+            }
+        } catch(Exception $e){
+            return response()->json([
+                'status' => 'error',
+                'message' => 'An error occurred while sending the medications to the Arduino'
+            ]);
+        }
+    }
+
+    private function sendCommandToArduino(){
+
+        // $arduinoIpAddress = 'ARDUINO-IP';
+        
+        // $client = new Client();
+        // $response = $client->get("http://{$arduinoIpAddress}/ring-buzzer");
+
+        // $statusCode = $response->getStatusCode();
     }
 }
