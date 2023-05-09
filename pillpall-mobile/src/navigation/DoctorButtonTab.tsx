f8c92d4d5c6c35581ab43faddb3a2e0dd601cae7
RashaM <rashamassoud1998@gmail.com>
2023-05-09 16:15:51 +0300

    Frontend: Updated the navbar images

--- pillpall-mobile/src/navigation/DoctorButtonTab.tsx
+++ pillpall-mobile/src/navigation/DoctorButtonTab.tsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Image, Text} from "react-native";
+import { Image, Text } from "react-native";
+import { useIsFocused } from "@react-navigation/native";
 import { colors } from '../constants/palette';
 import ChangePassword from "../screens/changepassword";
 import Profile from "../screens/profile";
@@ -11,50 +12,42 @@ import { ChangePasswordStack } from '../navigation/DoctorStacks'
 
 const DoctorButtonTab = () => {
     const Tabs = createBottomTabNavigator();
+
     return (
-        <Tabs.Navigator 
-            screenOptions={{ headerShown: false,  
-                tabBarStyle: {backgroundColor: colors.blue, height: 92},
-                tabBarLabelStyle: { fontSize: 16, fontWeight: 'bold' }, 
+        <Tabs.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarStyle: { backgroundColor: colors.blue, height: 92 },
+                tabBarLabelStyle: { fontSize: 16, fontWeight: 'bold' },
                 tabBarActiveTintColor: 'white',
-            }}       
-        >            
-            <Tabs.Screen
-                options={{
-                    tabBarIcon: () => (
-                        <Image
-                            source={require('../../assets/navDoctor.png')}
-                            style={{ width: 32, height: 38 }}
-                        />
-                    ),
-                }}
-                name="Profile"
-                component={ProfileStack}
-            />
-            <Tabs.Screen
-            options={{
-                    tabBarIcon: () => (
-                        <Image
-                            source={require('../../assets/navUsers.png')}
-                            style={{ width: 80, height: 35 }}
-                        />
-                    ),
-                }}
-                name="Search"
-                component={PatientStack}
-            />
-            <Tabs.Screen
-                options={{
-                    tabBarIcon: () => (
-                        <Image
-                            source={require('../../assets/navSettings.png')}
-                            style={{ width: 40, height: 38 }}
-                        />
-                    ),
-                }}
-                name="Settings"
-                component={ChangePasswordStack}
-            />
+                tabBarIcon: ({ focused }) => {
+                    let imageSource;
+                    let imageStyle;
+
+                    if (route.name === 'Profile') {
+                        imageSource = focused
+                            ? require('../../assets/navDoctorFocused.png')
+                            : require('../../assets/navDoctor.png');
+                        imageStyle = { width: 32, height: 38 };
+                    } else if (route.name === 'Search') {
+                        imageSource = focused
+                            ? require('../../assets/navUsersFocused.png')
+                            : require('../../assets/navUsers.png');
+                        imageStyle = { width: 80, height: 35 };
+                    } else if (route.name === 'Settings') {
+                        imageSource = focused
+                            ? require('../../assets/navSettingsFocused.png')
+                            : require('../../assets/navSettings.png');
+                        imageStyle = { width: 40, height: 38 };
+                    }
+
+                    return <Image source={imageSource} style={imageStyle} />;
+                },
+            })}
+        >
+            <Tabs.Screen name="Profile" component={ProfileStack} />
+            <Tabs.Screen name="Search" component={PatientStack} />
+            <Tabs.Screen name="Settings" component={ChangePasswordStack} />
         </Tabs.Navigator>
     );
 };
