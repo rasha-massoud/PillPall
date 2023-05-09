e482ace516343e166db14dfc392756a0fd9db2f7
RashaM <rashamassoud1998@gmail.com>
2023-05-09 23:33:24 +0300

    Frontend: Created the AdminStacks.tsx file foe the naviation

---
+++ pillpall-mobile/src/navigation/AdminStacks.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Patients from '../screens/patients';
+import ApprovedDoctors from '../screens/approveddoctors';
+import UnapprovedDoctors from '../screens/unapproveddoctors';
+
+
+const Stack = createStackNavigator();
+
+export const PatientsStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: 'white' } }}>
+            <Stack.Screen name="Patients" component={Patients} />
+        </Stack.Navigator>
+    );
+};
+
+export const ApprovedDoctorsStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: 'white' } }}>
+            <Stack.Screen name="ApprovedDoctors" component={ApprovedDoctors} />
+        </Stack.Navigator>
+    );
+};
+
+export const UnapprovedDoctorsStack = () => {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: 'white' } }}>
+            <Stack.Screen name="UnapprovedDoctors" component={UnapprovedDoctors} />
+        </Stack.Navigator>
+    );
+};
+
