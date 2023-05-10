9bbfdfc45f4088db15e1a6f1b3f77f71d67a2a8c
RashaM <rashamassoud1998@gmail.com>
2023-05-10 21:13:33 +0300

    Frontend: Cleaned the Medical History Screen

--- pillpall-mobile/src/screens/medicalhistory/index.tsx
+++ pillpall-mobile/src/screens/medicalhistory/index.tsx
@@ -7,17 +7,10 @@ import { colors } from '../../constants/palette';
 import TextInputwithLabel from '../../components/TextInputwithLabel';
 import StepText from '../../components/StepText';
 import { useNavigation } from '@react-navigation/core';
-import { useDispatch, useSelector } from "react-redux";
-import { setChronicConditions, setPastSurgeries, setFamilyMedicalHistory, setAllergies, } from "../../store/slices/reportSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { setChronicConditions, setPastSurgeries, setFamilyMedicalHistory, setAllergies, } from '../../store/slices/reportSlice';
 
 import styles from './styles';
-
-interface MedicalHistoryData {
-  chronic_conditions: string;
-  past_surgeries: string;
-  family_medical_history: string;
-  allergies: string;
-}
 
 const MedicalHistory: FC = () => {
 
@@ -67,23 +60,23 @@ const MedicalHistory: FC = () => {
 
   return (
   
-  <SafeAreaView style={styles.container}>
-    <PageTitle title='Medical History' />
-    <StepText title='Step 5' color={colors.blue}></StepText>
+    <SafeAreaView style={styles.container}>
+      <PageTitle title='Medical History' />
+      <StepText title='Step 5' color={colors.blue} />
 
-    <Body1Text context="To provide personalized care, we'll need your medical history, which includes chronic conditions or illnesses, past surgeries or hospitalizations, family medical history, and allergies. This information is important for healthcare professionals to determine the patient's current health status, as well as to determine any potential risk factors for treatment."></Body1Text>
+      <Body1Text context="Unlock tailored care! Share your medical history to help healthcare professionals assess your current health and identify any potential treatment risks." />
 
-    <TextInputwithLabel label="Chronic Condition or Illness" placeholder='Enter your Chronic Condition or Illness if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handleChronicConditionsChange} />
+      <TextInputwithLabel label="Chronic Condition or Illness" placeholder='Enter your Chronic Condition or Illness if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handleChronicConditionsChange} />
 
-    <TextInputwithLabel label="Past Surgeries or Hospitalizations" placeholder='Enter your Past Surgeries or Hospitalizations if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handlePastSurgeriesChange} />
+      <TextInputwithLabel label="Past Surgeries or Hospitalizations" placeholder='Enter your Past Surgeries or Hospitalizations if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handlePastSurgeriesChange} />
 
-    <TextInputwithLabel label="Family Medical History" placeholder='Enter your Family Medical History if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handleFamilyMedicalHistoryChange} />
-    
-    <TextInputwithLabel label="Allergies" placeholder='Enter your Allergies if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handleAllergiesChange} />
+      <TextInputwithLabel label="Family Medical History" placeholder='Enter your Family Medical History if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handleFamilyMedicalHistoryChange} />
+      
+      <TextInputwithLabel label="Allergies" placeholder='Enter your Allergies if any' textinputprops={{ secureTextEntry: false }} onChangeText= {handleAllergiesChange} />
 
-    <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 40 }} buttonprops={{ title: "Continue", onPress: handleContinuePress }}  />
+      <CustomButton containerStyle={{ alignSelf: 'center', marginTop: 45, height: 45 }} buttonprops={{ title: "Continue", onPress: handleContinuePress }}  />
 
-  </SafeAreaView>
+    </SafeAreaView>
   );
 };
 
--- pillpall-mobile/src/screens/medicalhistory/styles.ts
+++ pillpall-mobile/src/screens/medicalhistory/styles.ts
@@ -1,10 +1,9 @@
 import { StyleSheet } from 'react-native';
-import { ViewStyle, TextStyle, ImageStyle } from 'react-native';
+import { ViewStyle } from 'react-native';
 import { Dimensions } from 'react-native';
 
 interface Styles {
     container: ViewStyle;
-    image: ImageStyle;
 }
 
 const { width, height } = Dimensions.get('window');
@@ -13,15 +12,9 @@ const LOGIN_PAGE_WIDTH = width * 0.9;
 const styles = StyleSheet.create<Styles>({
     container: {
         flex: 1,
-        marginTop: '28%',
+        marginTop: '32%',
         width: LOGIN_PAGE_WIDTH,
         marginHorizontal: width * 0.05, 
-    },
-    image: {
-        justifyContent: 'center',
-        marginBottom: 20,
-        width: '58%',
-        height: '30%',
     },
 });
 
