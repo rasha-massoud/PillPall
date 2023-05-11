352f94811f76028919338eb7cc67ce3271c66869
RashaM <rashamassoud1998@gmail.com>
2023-05-11 03:39:06 +0300

    Frontend: Cleaned the Patient Report Screen

--- pillpall-mobile/src/screens/patientreport/index.tsx
+++ pillpall-mobile/src/screens/patientreport/index.tsx
@@ -76,7 +76,6 @@ const PatientReport: FC<PatientReportProps> = ({route }) => {
             },
           }
         );
-        console.log(response.data);
         setAllResult(response.data);
       } catch (error) {
         console.error('An error occurred while getting the report', error);
