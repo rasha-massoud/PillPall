e50b7e89a040914981ede7acbaa983a0b976c4fc
RashaM <rashamassoud1998@gmail.com>
2023-05-16 15:39:12 +0300

    Frontend: Updated the IP Address based on the PC :ok_hand:

--- pillpall-mobile/src/constants/files.ts
+++ pillpall-mobile/src/constants/files.ts
@@ -1,3 +1,3 @@
-const FILE_URL = 'http://192.168.0.100:8000/';
+const FILE_URL = 'http://192.168.0.102:8000/';
 
 export default FILE_URL;
--- pillpall-mobile/src/constants/url.ts
+++ pillpall-mobile/src/constants/url.ts
@@ -1,3 +1,3 @@
-const API_URL = 'http://192.168.0.100:8000/api/v0.0.0/';
+const API_URL = 'http://192.168.0.102:8000/api/v0.0.0/';
 
 export default API_URL;
